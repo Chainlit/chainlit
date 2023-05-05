66eaa6010cbab935288e9ac5010750319cea356d
Willy Douhard <willy.douhard@gmail.com>
2023-05-05 15:00:28 +0200

    ci

--- examples/upload_file/main.py
+++ examples/upload_file/main.py
@@ -6,13 +6,15 @@ def init():
     file = None
 
     # Wait for the user to upload a file
+    print("Waiting for user to upload a file...")
     while file == None:
         file = cl.ask_for_file(
             title="Please upload a text file to begin!", accept=["text/plain"]
         )
-
+    print("Got file!", file.name)
     # Decode the file
     text = file.content.decode("utf-8")
+    print(text)
 
     # Let the user know that the system is ready
     cl.send_message(f"`{file.name}` uploaded, it contains {len(text)} characters!")
