63ede5b94b61048f492beb2dfb4b5fc40a2aed91
Willy Douhard <willy.douhard@gmail.com>
2023-06-01 12:38:25 +0200

    test that a file with the wrong extension gets rejected

--- cypress/e2e/ask_file/spec.cy.ts
+++ cypress/e2e/ask_file/spec.cy.ts
@@ -7,9 +7,10 @@ describe("Upload file", () => {
     cy.wait(["@settings"]);
   });
 
-  it("should be able to receive and decode the file", () => {
+  it("should be able to receive and decode files", () => {
     cy.get("#upload-button").should("exist");
 
+    // Upload a text file
     cy.fixture("state_of_the_union.txt", "utf-8").as("txtFile");
     cy.get("input[type=file]").selectFile("@txtFile", { force: true });
 
@@ -25,6 +26,14 @@ describe("Upload file", () => {
       );
 
     cy.get("#upload-button").should("exist");
+
+    // Expecting a python file, cpp file upload should be rejected
+    cy.fixture("hello.cpp", "utf-8").as("cppFile");
+    cy.get("input[type=file]").selectFile("@cppFile", { force: true });
+
+    cy.get(".message").should("have.length", 3);
+
+    // Upload a python file
     cy.fixture("hello.py", "utf-8").as("pyFile");
     cy.get("input[type=file]").selectFile("@pyFile", { force: true });
 
