bd7cbd9f82d2709b219ed18aa750ed47c28ab3de
Thibaut Patel <thibaut.patel@gmail.com>
2023-06-02 09:52:39 +0200

    perf: disable video on cypress tests by default (#27)
    This speeds up the CI (no video recording + no video compression), we can still enable the e2e video recording by reverting the flag locally.

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   e2e: {
     supportFile: false,
     defaultCommandTimeout: 10000,
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
