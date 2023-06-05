8956af9ec5c70c314f3e70b410e0893603492708
Thibaut Patel <thibaut.patel@gmail.com>
2023-06-05 14:04:07 +0200

    removed the iframe border around the pdf reader

--- src/chainlit/frontend/src/components/element/pdf.tsx
+++ src/chainlit/frontend/src/components/element/pdf.tsx
@@ -11,6 +11,7 @@ export default function PDFElement({ element }: Props) {
   return (
     <iframe
       src={element.url || `data:application/pdf;base64,${element.content}`}
+      style={{ border: 'none' }}
       width="100%"
       height="400px"
     ></iframe>
