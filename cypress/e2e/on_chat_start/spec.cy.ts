0996d296b31330c0c48c2765cbbf5373ed626bfa
Thibaut Patel <thibaut.patel@gmail.com>
2023-06-16 11:59:05 +0200

    tests: add code highlighting test

--- cypress/e2e/on_chat_start/spec.cy.ts
+++ cypress/e2e/on_chat_start/spec.cy.ts
@@ -12,5 +12,6 @@ describe("on_chat_start", () => {
     messages.should("have.length", 1);
 
     messages.eq(0).should("contain.text", "Hello!");
+    messages.eq(0).should("contain.html", "<div class=\"language-python\"");
   });
 });
