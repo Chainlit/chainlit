df14085ae59de26c8f2b68c1e886da7490bffedb
Thibaut Patel <thibaut.patel@gmail.com>
2023-06-20 14:18:57 +0200

    perf: move prism to dynamic import (#70)
    - This splits the code highlighting code into a new chunk
    - Speeds up initial loading, only loads code highlighting when needed

--- cypress/e2e/on_chat_start/main.py
+++ cypress/e2e/on_chat_start/main.py
@@ -3,4 +3,16 @@ import chainlit as cl
 
 @cl.on_chat_start
 async def start():
-    await cl.Message(content="Hello!").send()
+    await cl.Message(
+        content="""Hello!
+
+```python
+import chainlit as cl
+
+@cl.on_chat_start
+async def main():
+    await cl.Message(
+        content="Here is a simple message",
+    ).send()
+```"""
+    ).send()
--- src/chainlit/frontend/src/components/Code.tsx
+++ src/chainlit/frontend/src/components/Code.tsx
@@ -1,6 +1,6 @@
 import { Box, useTheme } from '@mui/material';
 import { CodeProps } from 'react-markdown/lib/ast-to-react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { PrismAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default function Code({ inline, children, ...props }: CodeProps) {
