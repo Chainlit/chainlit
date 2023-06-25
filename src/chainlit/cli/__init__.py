dd6b8eb215378948ad8954585587a31486ee66cc
Willy Douhard <willy.douhard@gmail.com>
2023-06-25 20:15:39 +0200

    debug ci

--- src/chainlit/cli/__init__.py
+++ src/chainlit/cli/__init__.py
@@ -2,6 +2,7 @@ import click
 import os
 import sys
 import uvicorn
+import asyncio
 
 from chainlit.config import (
     config,
@@ -52,7 +53,16 @@ def run_chainlit(target: str):
 
     log_level = "debug" if config.run.debug else "error"
 
-    uvicorn.run(app, host=host, port=port, log_level=log_level)
+    # Start the server
+    async def start():
+        config = uvicorn.Config(app, host=host, port=port, log_level=log_level)
+        server = uvicorn.Server(config)
+        await server.serve()
+
+    # Run the asyncio event loop instead of uvloop to enable re entrance
+    asyncio.run(start())
+
+    # uvicorn.run(app, host=host, port=port, log_level=log_level)
 
 
 # Define the "run" command for Chainlit CLI
