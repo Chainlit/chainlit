7ed512cc8a50e818e3f9a6098795741537a035c0
Thibaut Patel <thibaut.patel@gmail.com>
2023-07-06 16:45:01 +0200

    fix: add the RecoilRoot

--- src/chainlit/frontend/src/components/chat/message/content.spec.tsx
+++ src/chainlit/frontend/src/components/chat/message/content.spec.tsx
@@ -3,49 +3,54 @@ import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import MessageContent from './content';
 import { ITextElement } from 'state/element';
+import { RecoilRoot } from 'recoil';
 
 it('renders the message content', () => {
   const { getByText } = render(
-    <MessageContent
-      authorIsUser={false}
-      actions={[]}
-      elements={[]}
-      content="Hello World"
-    />
+    <RecoilRoot>
+      <MessageContent
+        authorIsUser={false}
+        actions={[]}
+        elements={[]}
+        content="Hello World"
+      />
+    </RecoilRoot>
   );
   expect(getByText('Hello World')).toBeInTheDocument();
 });
 
 it('highlights multiple sources correctly (no substring matching)', () => {
   const { getByText } = render(
-    <BrowserRouter>
-      <MessageContent
-        authorIsUser={false}
-        actions={[]}
-        elements={[
-          {
-            name: 'source_1',
-            type: 'text',
-            display: 'side',
-            content: 'source_1'
-          } as ITextElement,
-          {
-            name: 'source_12',
-            display: 'side',
-            type: 'text',
-            content: 'hi'
-          } as ITextElement,
-          {
-            name: 'source_121',
-            display: 'side',
-            type: 'text',
-            content: 'hi'
-          } as ITextElement
-        ]}
-        content={`Hello world
+    <RecoilRoot>
+      <BrowserRouter>
+        <MessageContent
+          authorIsUser={false}
+          actions={[]}
+          elements={[
+            {
+              name: 'source_1',
+              type: 'text',
+              display: 'side',
+              content: 'source_1'
+            } as ITextElement,
+            {
+              name: 'source_12',
+              display: 'side',
+              type: 'text',
+              content: 'hi'
+            } as ITextElement,
+            {
+              name: 'source_121',
+              display: 'side',
+              type: 'text',
+              content: 'hi'
+            } as ITextElement
+          ]}
+          content={`Hello world
         source_121, source_1, source_12`}
-      />
-    </BrowserRouter>
+        />
+      </BrowserRouter>
+    </RecoilRoot>
   );
   expect(getByText('source_1')).toBeInTheDocument();
   expect(getByText('source_12')).toBeInTheDocument();
