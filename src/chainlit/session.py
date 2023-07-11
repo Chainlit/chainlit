b25d299fd09469eb16c2346c5a712a6e82522c53
ykoeth <yann.koeth@gmail.com>
2023-07-11 17:03:12 +0200

    use a server generated session id

--- src/chainlit/session.py
+++ src/chainlit/session.py
@@ -1,3 +1,4 @@
+from secrets import token_urlsafe
 from typing import Dict, TypedDict, Optional, Callable, Any, Union
 from chainlit.client.base import BaseAuthClient, BaseDBClient
 from chainlit.types import AskResponse
@@ -26,4 +27,34 @@ class Session(TypedDict):
     db_client: Optional[BaseDBClient]
 
 
+def new_session(key: str, data: Session):
+    session_id = token_urlsafe()
+    session = data | {"id": session_id}
+    sessions[session_id] = session
+    sessions_id[key] = session_id
+    return session
+
+
+def restore_session(session: Session, new_key: str):
+    # TODO: Remove previous session key
+    # sessions_id = {k: v for k, v in sessions_id.items() if v != session["id"]}
+    sessions_id[new_key] = session["id"]
+
+
+def get_session(key: str):
+    session_id = sessions_id.get(key)
+    return get_session_by_id(session_id)
+
+
+def delete_session(key: str = None):
+    session_id = sessions_id.get(key)
+    sessions_id.pop(key)
+    sessions.pop(session_id)
+
+
+def get_session_by_id(session_id: str):
+    return sessions.get(session_id)
+
+
+sessions_id: Dict[str, str] = {}
 sessions: Dict[str, Session] = {}
