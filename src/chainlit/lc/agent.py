45fda4c169542e214aad90fb4d48a8feb9aca857
ramnes <contact@ramnes.eu>
2023-07-11 18:45:52 +0200

    Make run_langchain_agent more concise

--- src/chainlit/lc/agent.py
+++ src/chainlit/lc/agent.py
@@ -8,32 +8,18 @@ from chainlit.context import emitter_var
 
 
 async def run_langchain_agent(agent: Any, input_str: str, use_async: bool):
-    if use_async:
-        callback_handler = AsyncLangchainCallbackHandler()
-    else:
-        callback_handler = LangchainCallbackHandler()
-
     if hasattr(agent, "input_keys"):
         input_key = agent.input_keys[0]
-        if use_async:
-            raw_res = await agent.acall(
-                {input_key: input_str},
-                callbacks=[callback_handler],
-            )
-        else:
-            raw_res = await make_async(agent.__call__)(
-                {input_key: input_str}, callbacks=[callback_handler]
-            )
+        inputs = {input_key, input_str}
     else:
-        if use_async:
-            raw_res = await agent.acall(
-                input_str,
-                callbacks=[callback_handler],
-            )
-        else:
-            raw_res = await make_async(agent.__call__)(
-                input_str, callbacks=[callback_handler]
-            )
+        inputs = input_str
+
+    if use_async:
+        callback_handler = AsyncLangchainCallbackHandler()
+        raw_res = await agent.acall(inputs, callbacks=[callback_handler])
+    else:
+        callback_handler = LangchainCallbackHandler()
+        raw_res = await make_async(agent.__call__)(inputs, callbacks=[callback_handler])
 
     if hasattr(agent, "output_keys"):
         output_key = agent.output_keys[0]
