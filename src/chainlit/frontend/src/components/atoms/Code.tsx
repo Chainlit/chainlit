0f7760fdc92d20a3d9051760cd3da15514e91e53
Alim TUNC <alim@refty.co>
2023-07-19 11:44:36 +0200

    Add copy to clipboard button on code message

--- src/chainlit/frontend/src/components/atoms/Code.tsx
+++ src/chainlit/frontend/src/components/atoms/Code.tsx
@@ -2,57 +2,101 @@ import { grey } from 'palette';
 import { CodeProps } from 'react-markdown/lib/ast-to-react';
 import { PrismAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { useCopyToClipboard, useToggle } from 'usehooks-ts';
 
-import { Box, useTheme } from '@mui/material';
+import { CopyAll } from '@mui/icons-material';
+import { Box, IconButton, Tooltip } from '@mui/material';
+
+import useIsDarkMode from 'hooks/useIsDarkMode';
 
 export default function Code({ inline, children, ...props }: CodeProps) {
-  const theme = useTheme();
+  const [showTooltip, toggleTooltip] = useToggle();
+  const isDarkMode = useIsDarkMode();
+  const [value, copy] = useCopyToClipboard();
 
   const match = /language-(\w+)/.exec(props.className || '');
-  if (!inline && match) {
-    return (
-      <SyntaxHighlighter
-        {...props}
-        children={String(children).replace(/\n$/, '')}
-        style={dracula}
-        wrapLongLines
-        language={match[1]}
-        PreTag="div"
-      />
-    );
-  } else if (inline) {
-    return (
-      <code
-        {...props}
-        style={{
-          background: theme.palette.mode === 'dark' ? grey[800] : grey[200],
-          borderRadius: '4px',
-          padding: '0.2em 0.4em',
-          overflowX: 'auto'
-        }}
-      >
-        {children}
-      </code>
-    );
-  } else {
-    return (
-      <Box
-        sx={{
-          background: theme.palette.mode === 'dark' ? grey[800] : grey[200],
-          borderRadius: '4px',
-          padding: theme.spacing(1),
-          overflowX: 'auto'
-        }}
-      >
+  const showSyntaxHighlighter = !inline && match;
+
+  const renderCode = () => {
+    if (showSyntaxHighlighter) {
+      return (
+        <SyntaxHighlighter
+          {...props}
+          children={String(children).replace(/\n$/, '')}
+          style={dracula}
+          wrapLongLines
+          language={match[1]}
+          PreTag="div"
+        />
+      );
+    } else if (inline) {
+      return (
         <code
           {...props}
           style={{
-            whiteSpace: 'pre-wrap'
+            background: isDarkMode ? grey[800] : grey[200],
+            borderRadius: '4px',
+            padding: '0.2em 0.4em',
+            overflowX: 'auto'
           }}
         >
           {children}
         </code>
-      </Box>
-    );
-  }
+      );
+    } else {
+      return (
+        <Box
+          sx={{
+            background: isDarkMode ? grey[800] : grey[200],
+            borderRadius: '4px',
+            padding: (theme) => theme.spacing(1),
+            overflowX: 'auto'
+          }}
+        >
+          <code
+            {...props}
+            style={{
+              whiteSpace: 'pre-wrap'
+            }}
+          >
+            {children}
+          </code>
+        </Box>
+      );
+    }
+  };
+
+  return (
+    <code style={{ position: 'relative' }}>
+      <Tooltip
+        open={showTooltip}
+        title={'Copied to clipboard!'}
+        onClose={toggleTooltip}
+      >
+        <IconButton
+          sx={{
+            color: showSyntaxHighlighter || isDarkMode ? grey[200] : grey[800],
+            position: 'absolute',
+            right: 4,
+            top: 4,
+            zIndex: 1
+          }}
+          onClick={() => {
+            copy(children[0] as string)
+              .then(() => {
+                toggleTooltip();
+                console.log('Successfully copied: ', value);
+              })
+              .catch((err) =>
+                console.log('An error occurred while copying: ', err)
+              );
+          }}
+        >
+          <CopyAll />
+        </IconButton>
+      </Tooltip>
+
+      {renderCode()}
+    </code>
+  );
 }
