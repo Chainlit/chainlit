ed6aa6d279a3fbe40f554fe93f921121b5fd33cb
Mathias <mathias.spanhove@hotmail.be>
2023-07-22 18:45:36 +0200

    Use stack for cleaner implementation

--- src/chainlit/__init__.py
+++ src/chainlit/__init__.py
@@ -2,13 +2,13 @@ from dotenv import load_dotenv
 from typing import Callable, Any, Optional, TYPE_CHECKING
 import os
 import asyncio
-from chainlit.haystack import HAYSTACK_INSTALLED
 
 if TYPE_CHECKING:
     from chainlit.client.base import BaseDBClient, UserDict
 
 from chainlit.lc import LANGCHAIN_INSTALLED
 from chainlit.llama_index import LLAMA_INDEX_INSTALLED
+from chainlit.haystack import HAYSTACK_INSTALLED
 from chainlit.utils import wrap_user_function
 from chainlit.config import config
 from chainlit.telemetry import trace
@@ -47,7 +47,7 @@ if LLAMA_INDEX_INSTALLED:
 
 if HAYSTACK_INSTALLED:
     from chainlit.haystack.callbacks import (
-        HaystackCallbackHandler,
+        HaystackAgentCallbackHandler,
     )
 
 env_found = load_dotenv(dotenv_path=os.path.join(os.getcwd(), ".env"))
@@ -186,7 +186,7 @@ __all__ = [
     "LangchainCallbackHandler",
     "AsyncLangchainCallbackHandler",
     "LlamaIndexCallbackHandler",
-    "HaystackCallbackHandler",
+    "HaystackAgentCallbackHandler",
     "client_factory",
     "run_sync",
     "make_async",
--- src/chainlit/haystack/callbacks.py
+++ src/chainlit/haystack/callbacks.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Any, Optional, TypeVar, Generic
 
 import chainlit as cl
 from chainlit.config import config
@@ -8,8 +8,28 @@ from chainlit.emitter import ChainlitEmitter
 from haystack.agents.agent_step import AgentStep
 from haystack.agents import Agent, Tool
 
+T = TypeVar("T")
 
-class HaystackCallbackHandler:
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self.items: list[T] = []
+
+    def __len__(self):
+        return len(self.items)
+
+    def push(self, item: T) -> None:
+        self.items.append(item)
+
+    def pop(self) -> T:
+        return self.items.pop()
+
+    def peek(self) -> T:
+        return self.items[-1]
+
+
+class HaystackAgentCallbackHandler:
+    stack: Stack[cl.Message]
     emitter: ChainlitEmitter
 
     def __init__(self, agent: Agent):
@@ -22,47 +42,58 @@ class HaystackCallbackHandler:
         agent.tm.callback_manager.on_tool_finish += self.on_tool_finish
         agent.tm.callback_manager.on_tool_error += self.on_tool_error
 
-    def get_root_message_id(self):
+    def get_root_message(self):
         self.emitter = self.emitter if hasattr(self, "emitter") else get_emitter()
 
         if not self.emitter.session.root_message:
             root_message = cl.Message(author=config.ui.name, content="")
             cl.run_sync(root_message.send())
 
-        return self.emitter.session.root_message.id
+        return self.emitter.session.root_message
 
     def on_agent_start(self, **kwargs: Any) -> None:
         # Prepare agent step message for streaming
         self.agent_name = kwargs.get("name", "Agent")
-        self.agent_message = cl.Message(
-            author=self.agent_name, parent_id=self.get_root_message_id(), content=""
-        )
+        self.stack = Stack[cl.Message]()
+        self.stack.push(self.get_root_message())
 
+        agent_message = cl.Message(
+            author=self.agent_name, parent_id=self.stack.peek().id, content=""
+        )
+        self.stack.push(agent_message)
+
+    # This method is called when a step has finished
     def on_agent_step(self, agent_step: AgentStep, **kwargs: Any) -> None:
-        # Send previous (streamed) agent step message
-        cl.run_sync(self.agent_message.send())
+        # Send previous agent step message
+        self.latest_agent_message = self.stack.pop()
 
-        # Create next agent step message for streaming
-        self.previous_agent_message = self.agent_message
-        self.agent_message = cl.Message(
-            author=self.agent_name, parent_id=self.get_root_message_id(), content=""
-        )
+        # If token streaming is disabled
+        if self.latest_agent_message.content == "":
+            self.latest_agent_message.content = agent_step.prompt_node_response
 
-    def on_agent_finish(self, agent_step: AgentStep, **kwargs: Any):
-        # Send previous (streamed) agent step message
-        cl.run_sync(self.agent_message.send())
-        self.agent_message = None
-        self.previous_agent_message = None
+        cl.run_sync(self.latest_agent_message.send())
 
-    def on_new_token(self, token, **kwargs: Any):
+        if not agent_step.is_last():
+            # Prepare message for next agent step
+            agent_message = cl.Message(
+                author=self.agent_name, parent_id=self.stack.peek().id, content=""
+            )
+            self.stack.push(agent_message)
+
+    def on_agent_finish(self, agent_step: AgentStep, **kwargs: Any) -> None:
+        self.latest_agent_message = None
+        self.stack = None
+
+    def on_new_token(self, token, **kwargs: Any) -> None:
         # Stream agent step tokens
-        cl.run_sync(self.agent_message.stream_token(token))
+        cl.run_sync(self.stack.peek().stream_token(token))
 
-    def on_tool_start(self, tool_input: str, tool: Tool, **kwargs: Any):
+    def on_tool_start(self, tool_input: str, tool: Tool, **kwargs: Any) -> None:
         # Tool started, create message
-        self.tool_message = cl.Message(
-            author=tool.name, parent_id=self.previous_agent_message.id, content=""
+        tool_message = cl.Message(
+            author=tool.name, parent_id=self.latest_agent_message.id, content=""
         )
+        self.stack.push(tool_message)
 
     def on_tool_finish(
         self,
@@ -72,12 +103,11 @@ class HaystackCallbackHandler:
         **kwargs: Any
     ) -> None:
         # Tool finished, send message with tool_result
-        self.tool_message.content = tool_result
-        cl.run_sync(self.tool_message.send())
-        self.tool_message = None
+        tool_message = self.stack.pop()
+        tool_message.content = tool_result
+        cl.run_sync(tool_message.send())
 
-    def on_tool_error(self, exception: Exception, tool: Tool, **kwargs: Any):
+    def on_tool_error(self, exception: Exception, tool: Tool, **kwargs: Any) -> None:
         # Tool error, send error message
-        cl.run_sync(self.tool_message.remove())
+        cl.run_sync(self.stack.pop().remove())
         cl.run_sync(cl.ErrorMessage(str(exception), author=tool.name).send())
-        self.tool_message = None
