76e089a2ade5d1206b496945ef3ff49e9c78c9bf
Thibaut Patel <thibaut.patel@gmail.com>
2023-07-27 22:04:25 +0200

    add backend unit tests
    - The unit tests use pytest
    - Also fixes running mypy outside of a poetry shell
    - Also fixes a mypy error that prevented me to commit this change (local.py)

---
+++ src/tests/test_message.py
@@ -0,0 +1,36 @@
+import unittest.mock
+import uuid
+
+from chainlit.message import Message
+
+
+def fake_get_emitter():
+    FakeEmitter = unittest.mock.MagicMock()
+    return FakeEmitter
+
+
+@unittest.mock.patch("chainlit.message.get_emitter", new=fake_get_emitter)
+def test_Message_init():
+    test_content = "Test Content"
+    test_author = "Test Author"
+
+    msg = Message(content=test_content, author=test_author)
+
+    assert msg.content == test_content
+    assert msg.author == test_author
+    assert uuid.UUID(msg.id).version == 4
+    assert msg.emitter is not None
+
+
+@unittest.mock.patch("chainlit.message.get_emitter", new=fake_get_emitter)
+def test_Message_dict():
+    test_content = "Test Content"
+    test_author = "Test Author"
+
+    msg = Message(content=test_content, author=test_author)
+
+    msg2 = Message.from_dict(msg.to_dict())
+
+    assert msg.content == msg2.content
+    assert msg.author == msg2.author
+    assert msg.id == msg2.id
