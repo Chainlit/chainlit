d8c83736ff7336a62ebfcb3753976ab2aa8734e4
Alim TUNC <alim@refty.co>
2023-08-03 09:52:27 +0200

    Rework variable modal

--- src/chainlit/frontend/src/components/organisms/playground/editor/wrapper.tsx
+++ src/chainlit/frontend/src/components/organisms/playground/editor/wrapper.tsx
@@ -1,34 +1,42 @@
+import merge from 'lodash/merge';
 import { grey } from 'palette';
 import React from 'react';
 
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Stack, SxProps, Theme, Typography } from '@mui/material';
 
 interface Props {
   title: string;
+  sx?: SxProps<Theme>;
+  sxChildren?: SxProps<Theme>;
 }
 
 export default function EditorWrapper({
   children,
-  title
+  title,
+  sx,
+  sxChildren
 }: React.PropsWithChildren<Props>) {
   return (
-    <Stack spacing={1.5} sx={{ width: '100%' }}>
+    <Stack spacing={1.5} sx={merge({ width: '100%' }, sx)}>
       <Typography fontSize="14px" fontWeight={700} color={grey[400]}>
         {title}
       </Typography>
       <Box
-        sx={{
-          fontFamily: 'Inter',
-          fontSize: '16px',
-          lineHeight: '24px',
-          padding: 3,
-          border: (theme) => `1px solid ${theme.palette.divider}`,
-          borderRadius: '0.375rem',
-          overflowY: 'auto',
-          flexGrow: 1,
-          caretColor: (theme) => theme.palette.text.primary,
-          backgroundColor: (theme) => theme.palette.background.paper
-        }}
+        sx={merge(
+          {
+            fontFamily: 'Inter',
+            fontSize: '16px',
+            lineHeight: '24px',
+            padding: 3,
+            border: (theme: Theme) => `1px solid ${theme.palette.divider}`,
+            borderRadius: '0.375rem',
+            overflowY: 'auto',
+            flexGrow: 1,
+            caretColor: (theme: Theme) => theme.palette.text.primary,
+            backgroundColor: (theme: Theme) => theme.palette.background.paper
+          },
+          sxChildren
+        )}
       >
         {children}
       </Box>
