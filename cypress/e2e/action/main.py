f2b2c0d528a1c674384fb570fb130b3780ce6b31
chenjunqing <342115615@qq.com>
2023-08-08 16:09:54 +0800

    test(action): add unit tests for multiple actions

--- cypress/e2e/action/main.py
+++ cypress/e2e/action/main.py
@@ -12,11 +12,27 @@ async def on_action(action: cl.Action):
     await action.remove()
 
 
+@cl.action_callback("multiple actions")
+async def on_action(action: cl.Action):
+    await cl.Message(content=f"Action(id={action.id}) has been removed!").send()
+    await action.remove()
+
+
 @cl.on_chat_start
 async def main():
     actions = [
         cl.Action(name="test action", value="test"),
         cl.Action(name="removable action", value="test"),
         cl.Action(name="label action", value="test", label="Test Label"),
+        cl.Action(
+            name="multiple actions",
+            value="multiple action one",
+            label="multiple action one",
+        ),
+        cl.Action(
+            name="multiple actions",
+            value="multiple action two",
+            label="multiple action two",
+        ),
     ]
     await cl.Message("Hello, this is a test message!", actions=actions).send()
