b53bd471c0541789458e5042f27300fe71d6a1d1
Alim TUNC <alim@refty.co>
2023-08-18 17:22:15 +0200

    feat: Add new collapse component

---
+++ src/chainlit/frontend/src/components/atoms/Collapse.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useToggle } from 'usehooks-ts';
+
+import {
+  ArrowCircleDown,
+  ArrowCircleUp,
+  DownloadRounded
+} from '@mui/icons-material';
+import {
+  Box,
+  IconButton,
+  Collapse as MCollapse,
+  Stack,
+  Theme
+} from '@mui/material';
+
+interface CollapseProps {
+  children: React.ReactNode;
+  onDownload: () => void;
+}
+
+const Collapse = ({ children, onDownload }: CollapseProps): JSX.Element => {
+  const [expandAll, toggleExpandAll] = useToggle(false);
+
+  return (
+    <Box>
+      <MCollapse
+        sx={{
+          border: (theme: Theme) => `3px solid ${theme.palette.divider}`,
+          borderRadius: 1,
+          padding: 1
+        }}
+        in={expandAll}
+        collapsedSize={30}
+      >
+        {children}
+      </MCollapse>
+      <Stack
+        sx={{
+          position: 'absolute',
+          right: 0
+        }}
+        direction="row"
+      >
+        <IconButton onClick={toggleExpandAll}>
+          {expandAll ? <ArrowCircleUp /> : <ArrowCircleDown />}
+        </IconButton>
+        <IconButton onClick={onDownload}>
+          <DownloadRounded />
+        </IconButton>
+      </Stack>
+    </Box>
+  );
+};
+
+export default Collapse;
