5dc38c6a478d2f79fddde49a78139774d6dccac8
Alim TUNC <alim@refty.co>
2023-08-28 10:27:06 +0200

    refactor: Replace husky & lint-staged by lefthook

--- lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  "**/*.{js,jsx,ts,tsx}": ["npx prettier --write", "npx eslint --fix"],
-  "**/*.{ts,tsx}": [() => "tsc --skipLibCheck --noEmit"],
-  "**/*.py": ["black", "isort --profile=black"],
-};
