0b554d0da6f13af9bf2246fc84dfd907d3d1ae3a
Willy Douhard <willy.douhard@gmail.com>
2023-08-30 16:25:52 +0200

    add name to PromptMessage for openai function message

--- src/chainlit/prompt.py
+++ src/chainlit/prompt.py
@@ -13,11 +13,17 @@ class BaseTemplate(DataClassJsonMixin):
 
 @dataclass
 class PromptMessage(BaseTemplate):
+    # This is used for Langchain's MessagesPlaceholder
     placeholder_size: Optional[int] = None
+    # This is used for OpenAI's function message
+    name: Optional[str] = None
     role: Optional[Literal["system", "assistant", "user", "function"]] = None
 
     def to_openai(self):
-        return {"role": self.role, "content": self.formatted}
+        msg_dict = {"role": self.role, "content": self.formatted}
+        if self.role == "function":
+            msg_dict["name"] = self.name or ""
+        return msg_dict
 
     def to_string(self):
         return f"{self.role}: {self.formatted}"
