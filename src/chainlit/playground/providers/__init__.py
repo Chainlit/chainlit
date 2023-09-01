a6fc645a3cf59cb850a575b8c176a9401de81488
Willy Douhard <willy.douhard@gmail.com>
2023-09-01 20:20:35 +0200

    prevent langchain to be imported by default

--- src/chainlit/playground/providers/__init__.py
+++ src/chainlit/playground/providers/__init__.py
@@ -1,4 +1,3 @@
 from .anthropic import Anthropic
 from .huggingface import HFFlanT5
-from .langchain import LangchainGenericProvider
 from .openai import AzureChatOpenAI, AzureOpenAI, ChatOpenAI, OpenAI
