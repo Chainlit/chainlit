ede1f5dfe217eed851d3d8cd897a4a312c30ebf0
Alim TUNC <alim@refty.co>
2023-09-04 17:06:11 +0200

    fix: Conflict between shared dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -10,5 +11,16 @@ export default defineConfig({
     setupFiles: './setup-tests.ts',
     include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)']
   },
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      // To prevent conflicts with packages in @chainlit/components, we need to specify the resolution paths for these dependencies.
+      react: path.resolve(__dirname, './node_modules/react'),
+      '@mui/material': path.resolve(__dirname, './node_modules/@mui/material'),
+      '@mui/icons-material': path.resolve(
+        __dirname,
+        './node_modules/@mui/icons-material'
+      )
+    }
+  }
 });
