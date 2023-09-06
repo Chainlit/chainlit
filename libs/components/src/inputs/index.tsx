18d2cea67dfe767f10f64884b48eb9f58e180c03
Alim TUNC <alim@refty.co>
2023-09-06 11:05:55 +0200

    refactor: Move <SelectCategoryInput/> to @chainlit/components

--- libs/components/src/inputs/index.tsx
+++ libs/components/src/inputs/index.tsx
@@ -1,7 +1,8 @@
 export { InputLabel } from './InputLabel';
 export { InputStateHandler } from './InputStateHandler';
-export { TextInput } from './TextInput';
+export { SelectCategoryInput } from './selects/SelectCategoryInput';
 export { SelectInput } from './selects/SelectInput';
+export { TextInput } from './TextInput';
 
 export type { SelectInputProps, SelectItem } from './selects/SelectInput';
 export type { TextInputProps } from './TextInput';
