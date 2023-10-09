abb7e62322d9fcdf9ca746a1d0b0f87e416878a4
SuperTurk <alim@refty.co>
2023-10-09 19:30:39 +0200

    feat: Add test on chat settings (#462)

---
+++ cypress/e2e/chat_settings/main.py
@@ -0,0 +1,66 @@
+import chainlit as cl
+from chainlit.input_widget import Select, Slider, Switch
+
+
+@cl.on_chat_start
+async def start():
+    settings = await cl.ChatSettings(
+        [
+            Select(
+                id="Model",
+                label="OpenAI - Model",
+                values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
+                initial_index=0,
+            ),
+            Switch(id="Streaming", label="OpenAI - Stream Tokens", initial=True),
+            Slider(
+                id="Temperature",
+                label="OpenAI - Temperature",
+                initial=1,
+                min=0,
+                max=2,
+                step=0.1,
+            ),
+            Slider(
+                id="SAI_Steps",
+                label="Stability AI - Steps",
+                initial=30,
+                min=0,
+                max=150,
+                step=1,
+                description="Amount of inference steps performed on image generation.",
+            ),
+            Slider(
+                id="SAI_Cfg_Scale",
+                label="Stability AI - Cfg_Scale",
+                initial=7,
+                min=1,
+                max=35,
+                step=0.1,
+                description="Influences how strongly your generation is guided to match your prompt.",
+            ),
+            Slider(
+                id="SAI_Width",
+                label="Stability AI - Image Width",
+                initial=512,
+                min=0,
+                max=2048,
+                step=64,
+                tooltip="Measured in pixels",
+            ),
+            Slider(
+                id="SAI_Height",
+                label="Stability AI - Image Height",
+                initial=512,
+                min=0,
+                max=2048,
+                step=64,
+                tooltip="Measured in pixels",
+            ),
+        ]
+    ).send()
+
+
+@cl.on_settings_update
+async def setup_agent(settings):
+    await cl.Message(content="Settings updated!").send()
---
+++ cypress/e2e/chat_settings/spec.cy.ts
@@ -0,0 +1,51 @@
+import { runTestServer } from '../../support/testUtils';
+
+describe('Customize chat settings', () => {
+  before(() => {
+    runTestServer();
+  });
+
+  it('should update inputs', () => {
+    // Open chat settings modal
+    cy.get('#chat-settings-open-modal').should('exist');
+    cy.get('#chat-settings-open-modal').click();
+    cy.get('#chat-settings-dialog').should('exist');
+
+    // Update inputs
+    cy.get('#mui-component-select-Model').click();
+    cy.contains('gpt-4').click();
+    cy.get('#Model').should('have.value', 'gpt-4');
+
+    cy.get('#Temperature').clear().type('0.4');
+    cy.get('#Temperature').should('have.value', '0.4');
+
+    cy.get('#SAI_Steps').clear().type('5');
+    cy.get('#SAI_Steps').should('have.value', '50');
+
+    cy.get('#SAI_Cfg_Scale').clear().type('2');
+    cy.get('#SAI_Cfg_Scale').should('have.value', '20');
+
+    cy.get('#SAI_Width').clear().type('140');
+    cy.get('#SAI_Width').should('have.value', '1400');
+
+    cy.get('#SAI_Height').clear().type('140');
+    cy.get('#SAI_Height').should('have.value', '1400');
+
+    cy.contains('Confirm').click();
+
+    cy.get('.message').should('have.length', 1);
+    cy.get('.message').eq(0).should('contain', 'Settings updated!');
+
+    // Check if inputs are updated
+    cy.get('#chat-settings-open-modal').click();
+    cy.get('#Temperature').should('have.value', '0.4');
+    cy.get('#SAI_Steps').should('have.value', '50');
+    cy.get('#SAI_Cfg_Scale').should('have.value', '20');
+    cy.get('#SAI_Width').should('have.value', '1400');
+    cy.get('#SAI_Height').should('have.value', '1400');
+
+    // Check if modal is correctly closed
+    cy.contains('Cancel').click();
+    cy.get('#chat-settings-dialog').should('not.exist');
+  });
+});
