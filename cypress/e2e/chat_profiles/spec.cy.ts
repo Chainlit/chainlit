03655e06aebcbf64d7daba963edd41057d66db05
Thibaut Patel <thibaut.patel@gmail.com>
2023-10-12 22:51:36 +0200

    another attempt, getting closer to the password_auth test

--- cypress/e2e/chat_profiles/spec.cy.ts
+++ cypress/e2e/chat_profiles/spec.cy.ts
@@ -3,16 +3,15 @@ import { runTestServer } from '../../support/testUtils';
 describe('Chat profiles', () => {
   before(() => {
     runTestServer();
+  });
+
+  it('should be able to select a chat profile', () => {
     cy.visit('/');
     cy.get("input[name='email']").type('admin');
     cy.get("input[name='password']").type('admin');
     cy.get("button[type='submit']").click();
     cy.get('.MuiAlert-message').should('not.exist');
     cy.get('#chat-input').should('exist');
-  });
-
-  it('should be able to select a chat profile', () => {
-    cy.get('.message').should('exist'); // temp fix as the CI isn't finding the next elements
 
     cy.get('[data-test="chat-profile:GPT-3.5"]').should('exist');
     cy.get('[data-test="chat-profile:GPT-4"]').should('exist');
