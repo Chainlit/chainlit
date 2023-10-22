f8e649a1f7fd59abc0db3251863b12ee08b4894d
Robert Nasuti <A088751@progressive.com>
2023-10-22 15:45:15 -0600

    Fix bug in playground for Azure OpenAI (#493)
    * Fix bug in playground for Azure OpenAI
    
    * Update openai.py
    
    Use hasattr instead of checking for choices in response

--- backend/chainlit/playground/providers/openai.py
+++ backend/chainlit/playground/providers/openai.py
@@ -145,8 +145,11 @@ class ChatOpenAIProvider(BaseProvider):
 
         async def create_event_stream():
             async for stream_resp in response:
-                token = stream_resp.choices[0]["delta"].get("content", "")
-                yield token
+                if hasattr(stream_resp, 'choices') and len(stream_resp.choices) > 0:
+                    token = stream_resp.choices[0]["delta"].get("content", "")
+                    yield token
+                else:
+                    continue
 
         return StreamingResponse(create_event_stream())
 
