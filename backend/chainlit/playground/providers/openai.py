7127659c03219c8a27cab9e1060f538e436bf3b6
fvaleye <florian.valeye@gmail.com>
2023-11-30 10:18:12 +0100

    feat(playground): add gpt-4-turbo in the playground

--- backend/chainlit/playground/providers/openai.py
+++ backend/chainlit/playground/providers/openai.py
@@ -362,7 +362,7 @@ ChatOpenAI = ChatOpenAIProvider(
         Select(
             id="model",
             label="Model",
-            values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
+            values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k", "gpt-4-1106-preview"],
             initial_value="gpt-3.5-turbo",
         ),
         *openai_common_inputs,
