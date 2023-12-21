13cd21a4f30ada3ccd5f7d501ab6a1b1898ac603
fvaleye <florian.valeye@gmail.com>
2023-12-21 15:43:41 +0100

    feat(playground): Add Gemini Pro in the LLM playground

--- backend/chainlit/playground/providers/vertexai.py
+++ backend/chainlit/playground/providers/vertexai.py
@@ -33,10 +33,10 @@ class ChatVertexAIProvider(BaseProvider):
 
         self.validate_env(request=request)
 
-        llm_settings = request.prompt.settings
+        llm_settings = request.generation.settings
         self.require_settings(llm_settings)
 
-        prompt = self.create_prompt(request)
+        messages = self.create_generation(request)
         model_name = llm_settings["model"]
         if model_name.startswith("chat-"):
             model = ChatModel.from_pretrained(model_name)
@@ -52,7 +52,7 @@ class ChatVertexAIProvider(BaseProvider):
 
         async def create_event_stream():
             for response in await cl.make_async(chat.send_message_streaming)(
-                prompt[0].formatted, **llm_settings
+                messages, **llm_settings
             ):
                 yield response.text
 
@@ -66,10 +66,10 @@ class GenerationVertexAIProvider(BaseProvider):
 
         self.validate_env(request=request)
 
-        llm_settings = request.prompt.settings
+        llm_settings = request.generation.settings
         self.require_settings(llm_settings)
 
-        prompt = self.create_prompt(request)
+        messages = self.create_generation(request)
         model_name = llm_settings["model"]
         if model_name.startswith("text-"):
             model = TextGenerationModel.from_pretrained(model_name)
@@ -84,9 +84,38 @@ class GenerationVertexAIProvider(BaseProvider):
 
         async def create_event_stream():
             for response in await cl.make_async(model.predict_streaming)(
-                prompt, **llm_settings
+                messages, **llm_settings
             ):
                 yield response.text
+
+        return StreamingResponse(create_event_stream())
+
+
+class GeminiProvider(BaseProvider):
+    async def create_completion(self, request):
+        await super().create_completion(request)
+        from vertexai.preview.generative_models import GenerativeModel
+        from google.cloud import aiplatform
+        import os
+
+        self.validate_env(request=request)
+
+        llm_settings = request.generation.settings
+        self.require_settings(llm_settings)
+
+        messages = self.create_generation(request)
+        aiplatform.init(  # TODO: remove this when Gemini is released in all the regions
+            project=os.environ["GCP_PROJECT_ID"],
+            location='us-central1',
+        )
+        model = GenerativeModel(llm_settings["model"])
+        del llm_settings["model"]
+
+        async def create_event_stream():
+            for response in await cl.make_async(model.generate_content)(
+                messages, stream=True, generation_config=llm_settings
+            ):
+                yield response.candidates[0].content.parts[0].text
 
         return StreamingResponse(create_event_stream())
 
@@ -119,6 +148,22 @@ GenerationVertexAI = GenerationVertexAIProvider(
             label="Model",
             values=["text-bison", "code-bison"],
             initial_value="text-bison",
+        ),
+        *vertexai_common_inputs,
+    ],
+    is_chat=False,
+)
+
+Gemini = GeminiProvider(
+    id="gemini",
+    env_vars=gcp_env_vars,
+    name="Gemini",
+    inputs=[
+        Select(
+            id="model",
+            label="Model",
+            values=["gemini-pro", "gemini-pro-vision"],
+            initial_value="gemini-pro",
         ),
         *vertexai_common_inputs,
     ],
