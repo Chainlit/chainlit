f3771fb809f2757ebc9ed367d809592415f1e2b4
Willy Douhard <willy.douhard@gmail.com>
2024-01-05 16:02:41 +0100

    fix tests

--- cypress/e2e/avatar/spec.cy.ts
+++ cypress/e2e/avatar/spec.cy.ts
@@ -8,11 +8,11 @@ describe('Avatar', () => {
   it('should be able to display avatars', () => {
     cy.get('.step').should('have.length', 5);
 
-    cy.get('.step').eq(0).find('.message-avatar').should('have.length', 0);
-    cy.get('.step').eq(1).find('.message-avatar').should('have.length', 1);
-    cy.get('.step').eq(2).find('.message-avatar').should('have.length', 0);
-    cy.get('.step').eq(3).find('.message-avatar').should('have.length', 1);
-    cy.get('.step').eq(4).find('.message-avatar').should('have.length', 1);
+    cy.get('.step').eq(0).find('img').should('have.length', 0);
+    cy.get('.step').eq(1).find('img').should('have.length', 1);
+    cy.get('.step').eq(2).find('img').should('have.length', 0);
+    cy.get('.step').eq(3).find('img').should('have.length', 1);
+    cy.get('.step').eq(4).find('img').should('have.length', 1);
 
     cy.get('.element-link').should('have.length', 0);
   });
