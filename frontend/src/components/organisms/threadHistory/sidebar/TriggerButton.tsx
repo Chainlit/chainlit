924189f0966ca4de11d37a37ac435a4007817527
Willy Douhard <willy.douhard@gmail.com>
2024-01-06 11:08:52 +0100

    add tooltip to sidebar trigger

--- frontend/src/components/organisms/threadHistory/sidebar/TriggerButton.tsx
+++ frontend/src/components/organisms/threadHistory/sidebar/TriggerButton.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-
 import Box from '@mui/material/Box';
+import Tooltip from '@mui/material/Tooltip';
 
 interface TriggerButtonProps {
   onClick: () => void;
@@ -16,26 +15,28 @@ const commonBoxStyles = {
 
 const TriggerButton = ({ onClick, open }: TriggerButtonProps): JSX.Element => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        cursor: 'pointer',
-        padding: 2,
-        '&:hover div:nth-child(1)': {
-          transform: open ? 'rotate(15deg)' : 'rotate(-15deg)'
-        },
-        '&:hover div:nth-child(2)': {
-          transform: open ? 'rotate(-15deg)' : 'rotate(15deg)'
-        }
-      }}
-      onClick={onClick}
-    >
-      <Box sx={{ ...commonBoxStyles, marginTop: '0.25rem' }} />
-      <Box sx={{ ...commonBoxStyles, marginTop: '-0.25rem' }} />
-    </Box>
+    <Tooltip title={open ? 'Close sidebar' : 'Open sidebar'} placement="right">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          cursor: 'pointer',
+          padding: 2,
+          '&:hover div:nth-child(1)': {
+            transform: open ? 'rotate(15deg)' : 'rotate(-15deg)'
+          },
+          '&:hover div:nth-child(2)': {
+            transform: open ? 'rotate(-15deg)' : 'rotate(15deg)'
+          }
+        }}
+        onClick={onClick}
+      >
+        <Box sx={{ ...commonBoxStyles, marginTop: '0.25rem' }} />
+        <Box sx={{ ...commonBoxStyles, marginTop: '-0.25rem' }} />
+      </Box>
+    </Tooltip>
   );
 };
 
