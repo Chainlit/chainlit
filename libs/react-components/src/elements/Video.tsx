a9c10fa7850ca0cdc09f0aac871df4039600e6da
happy-devs <andy@postonsunday.com>
2024-01-23 00:26:23 +0000

    Update Video.tsx

--- libs/react-components/src/elements/Video.tsx
+++ libs/react-components/src/elements/Video.tsx
@@ -5,12 +5,25 @@ const VideoElement = ({ element }: { element: IVideoElement }) => {
     return null;
   }
 
+  const url = new URL(element.url);
+
+  const renderIframe = (src: string) => {
+    return <iframe className={`${element.display}-video`} width="100%" src={src} allowFullScreen></iframe>;
+  };
+
+  if (url.hostname.includes('youtube.com') || url.hostname.includes('youtu.be')) {
+    const videoId = url.hostname.includes('youtu.be') ? url.pathname.split('/')[1] : url.searchParams.get('v');
+    const youtubeSrc = `https://www.youtube.com/embed/${videoId}?${element.start ? `start=${element.start}&` : ''}${element.autoplay ? 'autoplay=1' : ''}`;
+    
+    return renderIframe(youtubeSrc);
+  }
+
   return (
-    <video
-      className={`${element.display}-video`}
-      width="100%"
-      controls
-      src={element.url}
+    <video 
+      className={`${element.display}-video`} 
+      width="100%" 
+      controls 
+      src={element.start ? `${element.url}#t=${element.start}` : element.url}
     ></video>
   );
 };
