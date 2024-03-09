661b8c0ac443839855bbb27f9ab95d789e20f1ff
San Nguyen <vinhsannguyen91@gmail.com>
2024-03-09 23:05:32 +0900

    revert literalai api
    Signed-off-by: San Nguyen <vinhsannguyen91@gmail.com>

--- backend/chainlit/data/__init__.py
+++ backend/chainlit/data/__init__.py
@@ -1,7 +1,6 @@
 import os
 from typing import Optional
 
-from chainlit.config import config
 from chainlit.data.base import BaseDataLayer
 from chainlit.data.chainlit import ChainlitDataLayer
 from chainlit.data.mongodb import MongoDBDataLayer
@@ -9,16 +8,9 @@ from chainlit.data.mongodb import MongoDBDataLayer
 
 _data_layer: Optional[BaseDataLayer] = None
 
-if config.data_layer.database == "chainlit":
-    api_key = os.environ.get("LITERAL_API_KEY")
-    assert api_key is not None
+if api_key := os.environ.get("LITERAL_API_KEY"):
     server = os.environ.get("LITERAL_SERVER")
     _data_layer = ChainlitDataLayer(api_key=api_key, server=server)
-if config.data_layer.database == "mongodb":
-    if config.data_layer.object_storage != "s3":
-        raise ValueError("MongoDB data layer requires an S3 object storage")
-    db_url = os.environ.get("CHAINLIT_MONGODB_URL")
-    _data_layer = MongoDBDataLayer(db_url)
 
 
 def get_data_layer():
