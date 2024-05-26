33d8567a4c32c3ffcc13b23a1a7ca60b0b42e9a2
San Nguyen <vinhsannguyen91@gmail.com>
2024-05-26 09:43:53 +0900

    fix tests
    Signed-off-by: San Nguyen <vinhsannguyen91@gmail.com>

--- backend/chainlit/server.py
+++ backend/chainlit/server.py
@@ -153,9 +153,9 @@ def get_build_dir(local_target: str, packaged_target: str):
     packaged_build_dir = os.path.join(BACKEND_ROOT, packaged_target, "dist")
 
     if config.ui.custom_build and os.path.exists(
-        os.path.join(APP_ROOT, config.ui.custom_build, packaged_target, "dist")
+        os.path.join(APP_ROOT, config.ui.custom_build)
     ):
-        return os.path.join(APP_ROOT, config.ui.custom_build, packaged_target, "dist")
+        return os.path.join(APP_ROOT, config.ui.custom_build)
     elif os.path.exists(local_build_dir):
         return local_build_dir
     elif os.path.exists(packaged_build_dir):
