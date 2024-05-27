0d9b1388965cd7dc97b7788b407eed111fbcd01f
Willy Douhard <willy.douhard@gmail.com>
2024-05-27 13:56:07 +0200

    use chat profile icon for avatar

--- frontend/src/components/molecules/messages/components/Avatar.tsx
+++ frontend/src/components/molecules/messages/components/Avatar.tsx
@@ -1,8 +1,12 @@
+import { useMemo } from 'react';
 import { useRecoilValue } from 'recoil';
 
 import Avatar from '@mui/material/Avatar';
 
+import { useChatSession } from '@chainlit/react-client';
+
 import { apiClientState } from 'state/apiClient';
+import { projectSettingsState } from 'state/project';
 
 interface Props {
   author: string;
@@ -10,8 +14,24 @@ interface Props {
 }
 
 const MessageAvatar = ({ author, hide }: Props) => {
+  const { chatProfile } = useChatSession();
+  const pSettings = useRecoilValue(projectSettingsState);
+
+  const selectedChatProfile = useMemo(() => {
+    return pSettings?.chatProfiles.find(
+      (profile) => profile.name === chatProfile
+    );
+  }, [pSettings, chatProfile]);
+
   const apiClient = useRecoilValue(apiClientState);
-  const url = apiClient?.buildEndpoint(`/avatars/${author || 'default'}`);
+
+  const avatarUrl = useMemo(() => {
+    const isAssistant = !author || author === pSettings?.ui.name;
+    if (isAssistant && selectedChatProfile?.icon) {
+      return selectedChatProfile.icon;
+    }
+    return apiClient?.buildEndpoint(`/avatars/${author || 'default'}`);
+  }, [apiClient, selectedChatProfile, pSettings, author]);
 
   return (
     <span className={`message-avatar`}>
@@ -21,7 +41,7 @@ const MessageAvatar = ({ author, hide }: Props) => {
           height: '1.6rem',
           bgcolor: 'transparent'
         }}
-        src={hide ? undefined : url}
+        src={hide ? undefined : avatarUrl}
       />
     </span>
   );
