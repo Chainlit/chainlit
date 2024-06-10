904acb049e882366081b49d131a0e3218c5a6ad1
Aniruddha Adhikary <aniruddha_adhikary@tech.gov.sg>
2024-06-10 09:38:00 +0800

    chore: add in-memory TTL cache

---
+++ backend/chainlit/auth_ext.py
@@ -0,0 +1,9 @@
+import cachetools
+from chainlit.config import config
+
+# This blacklist blocks out JWT Tokens when users click "Log out" / "Sign out" manually
+jwt_blacklist = cachetools.TTLCache(
+    maxsize=8096, ttl=config.project.user_session_timeout
+)
+
+print("config.project.user_session_timeout=", config.project.user_session_timeout)
