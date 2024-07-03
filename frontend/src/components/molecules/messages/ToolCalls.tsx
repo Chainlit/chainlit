4cc4928e3b9a88a957e1c5d2d5d538b4174a39ce
Willy Douhard <willy.douhard@gmail.com>
2024-07-03 12:44:12 +0200

    remove dead code

--- frontend/src/components/molecules/messages/ToolCalls.tsx
+++ frontend/src/components/molecules/messages/ToolCalls.tsx
@@ -12,44 +12,9 @@ interface Props {
   isRunning?: boolean;
 }
 
-function groupToolSteps(step: IStep): IStep[][] {
-  const groupedSteps: IStep[][] = [];
-  let currentGroup: IStep[] = [];
-
-  function processStep(currentStep: IStep) {
-    if (currentStep.type === 'tool') {
-      if (
-        currentGroup.length === 0 ||
-        currentGroup[0].name === currentStep.name
-      ) {
-        currentGroup.push(currentStep);
-      } else {
-        groupedSteps.push(currentGroup);
-        currentGroup = [currentStep];
-      }
-    } else if (currentStep.steps) {
-      // If we haven't found any tools yet, recurse into the steps
-      if (groupedSteps.length === 0 && currentGroup.length === 0) {
-        for (const childStep of currentStep.steps) {
-          processStep(childStep);
-        }
-      }
-    }
-  }
-
-  processStep(step);
-
-  // Push the last group if it exists
-  if (currentGroup.length > 0) {
-    groupedSteps.push(currentGroup);
-  }
-
-  return groupedSteps;
-}
-
 export default function ToolCalls({ message, elements, isRunning }: Props) {
   const toolCalls = useMemo(() => {
-    return message.steps ? groupToolSteps(message) : [];
+    return message.steps ? message.steps.filter((s) => s.type === 'tool') : [];
   }, [message]);
 
   if (!toolCalls.length) {
@@ -58,14 +23,7 @@ export default function ToolCalls({ message, elements, isRunning }: Props) {
 
   return (
     <Stack width="100%" direction="column" gap={1}>
-      {toolCalls.map((toolCalls, index) => (
-        <ToolCall
-          key={index}
-          steps={toolCalls}
-          elements={elements}
-          isRunning={isRunning}
-        />
-      ))}
+      <ToolCall steps={toolCalls} elements={elements} isRunning={isRunning} />
     </Stack>
   );
 }
