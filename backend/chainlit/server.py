Chainlit/chainlit

commit 8ecd415834f8ac7bb44f233e76106259b7a02560

fix tool nesting

backend/chainlit/server.py
@@ -332,7 +332,7 @@ def get_user_facing_url(url: URL):
     if config_url.path.endswith("/"):
         config_url = config_url.replace(path=config_url.path[:-1])
 
-    return config_url.__str__() + url.path
+    return config_url.__str__()
 
 
 @router.get("/auth/config")

cypress/e2e/step/spec.cy.ts
@@ -14,9 +14,9 @@ describeSyncAsync('Step', () => {
 
     cy.get('#tool-call-tool1').should('exist').click();
 
-    cy.get('#tool-call-tool2').should('exist').click();
+    cy.get('#tool-call-tool2').should('not.exist');
 
-    cy.get('#tool-call-tool3').should('exist').click();
+    cy.get('#tool-call-tool3').should('not.exist');
 
     cy.get('.step').should('have.length', 2);
   });

cypress/e2e/streaming/main.py
@@ -27,10 +27,3 @@ async def main():
         await cl.sleep(0.2)
 
     await step.send()
-
-    step = cl.Step(type="tool", name="tool2")
-    for seq in sequence_list:
-        await step.stream_token(token=seq, is_sequence=True)
-        await cl.sleep(0.2)
-
-    await step.send()

cypress/e2e/streaming/spec.cy.ts
@@ -36,8 +36,6 @@ describe('Streaming', () => {
 
     toolStream('tool1');
 
-    toolStream('tool2');
-
     cy.get('.step').should('have.length', 3);
   });
 });

frontend/src/components/molecules/messages/Message.tsx
@@ -3,7 +3,6 @@ import { MessageContext } from 'contexts/MessageContext';
 import { memo, useContext } from 'react';
 
 import Box from '@mui/material/Box';
-import Skeleton from '@mui/material/Skeleton';
 import Stack from '@mui/material/Stack';
 
 import { AskUploadButton } from './components/AskUploadButton';
@@ -40,7 +39,6 @@ const Message = memo(
       onError
     } = useContext(MessageContext);
     const layoutMaxWidth = useLayoutMaxWidth();
-
     const isAsk = message.waitForAnswer;
     const isUserMessage = message.type === 'user_message';
 
@@ -94,14 +92,10 @@ const Message = memo(
                     direction="row"
                     gap="1rem"
                     alignItems="center"
-                    my={0.5}
+                    my={2}
                     width="100%"
                   >
-                    <Skeleton
-                      variant="circular"
-                      width="1.6rem"
-                      height="1.6rem"
-                    />
+                    <MessageAvatar />
                     <BlinkingCursor />
                   </Stack>
                 )}

frontend/src/components/molecules/messages/Messages.tsx
@@ -17,8 +17,6 @@ const Messages = memo(
   ({ messages, elements, actions, indent, isRunning }: Props) => {
     const messageContext = useContext(MessageContext);
 
-    const isRoot = indent === 0;
-
     const filtered = messages.filter((m, i) => {
       const content = m.output;
       const hasContent = !!content;
@@ -38,7 +36,7 @@ const Messages = memo(
         (!hasContent && messageRunning)
       );
     });
-
+    console.log(messages);
     return (
       <>
         {filtered.map((m, i) => {
@@ -51,11 +49,9 @@ const Messages = memo(
           const showAvatar = typeIsDifferent || authorIsDifferent;
 
           const isLast = filtered.length - 1 === i;
-          let messageRunning =
+          const messageRunning =
             isRunning === undefined ? messageContext.loading : isRunning;
-          if (isRoot) {
-            messageRunning = messageRunning && isLast;
-          }
+
           return (
             <Message
               message={m}

frontend/src/components/molecules/messages/ToolCalls.tsx
@@ -12,12 +12,11 @@ interface Props {
   isRunning?: boolean;
 }
 
-function groupToolSteps(step: IStep) {
+function groupToolSteps(step: IStep): IStep[][] {
   const groupedSteps: IStep[][] = [];
-
   let currentGroup: IStep[] = [];
 
-  function traverseAndGroup(currentStep: IStep) {
+  function processStep(currentStep: IStep) {
     if (currentStep.type === 'tool') {
       if (
         currentGroup.length === 0 ||
@@ -26,19 +25,19 @@ function groupToolSteps(step: IStep) {
         currentGroup.push(currentStep);
       } else {
         groupedSteps.push(currentGroup);
-
         currentGroup = [currentStep];
       }
-    }
-
-    if (currentStep.steps) {
-      for (const childStep of currentStep.steps) {
-        traverseAndGroup(childStep);
+    } else if (currentStep.steps) {
+      // If we haven't found any tools yet, recurse into the steps
+      if (groupedSteps.length === 0 && currentGroup.length === 0) {
+        for (const childStep of currentStep.steps) {
+          processStep(childStep);
+        }
       }
     }
   }
 
-  traverseAndGroup(step);
+  processStep(step);
 
   // Push the last group if it exists
   if (currentGroup.length > 0) {
@@ -59,10 +58,10 @@ export default function ToolCalls({ message, elements, isRunning }: Props) {
 
   return (
     <Stack width="100%" direction="column" gap={1}>
-      {toolCalls.map((toolCall, index) => (
+      {toolCalls.map((toolCalls, index) => (
         <ToolCall
           key={index}
-          steps={toolCall}
+          steps={toolCalls}
           elements={elements}
           isRunning={isRunning}
         />

frontend/src/components/molecules/messages/components/Avatar.tsx
@@ -10,7 +10,7 @@ import {
 } from '@chainlit/react-client';
 
 interface Props {
-  author: string;
+  author?: string;
   hide?: boolean;
 }
 

commit 3836af23754862f7921476616441bcde3fa6b173

Fix Azure authentication
The callback path for authentication in Azure AD, and possibly others, was
unintentionally broken in 8ecd415 by modifying the user-facing path.

This commit reverts one line of that change to restore it to its previous
state.

backend/chainlit/server.py
@@ -332,7 +332,7 @@ def get_user_facing_url(url: URL):
     if config_url.path.endswith("/"):
         config_url = config_url.replace(path=config_url.path[:-1])
 
-    return config_url.__str__()
+    return config_url.__str__() + url.path
 
 
 @router.get("/auth/config")