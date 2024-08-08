525ddedb1ea9f1a68ef4a071d4b0fdbe0d48e64c
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-08-08 11:11:36 +0200

    Use Jinja2 template to render index template.
    Co-authored-by: dhruvsyos <dhruv@synergyos.ai>

--- backend/chainlit/config.py
+++ backend/chainlit/config.py
@@ -124,6 +124,9 @@ cot = "full"
 # Specify a custom meta image url.
 # custom_meta_image_url = "https://chainlit-cloud.s3.eu-west-3.amazonaws.com/logo/chainlit_banner.png"
 
+# Specify a custom meta url.
+# custom_url = ""
+
 # Specify a custom build directory for the frontend.
 # This can be used to customize the frontend code.
 # Be careful: If this is a relative path, it should not start with a slash.
@@ -259,6 +262,8 @@ class UISettings(DataClassJsonMixin):
     custom_font: Optional[str] = None
     # Optional custom meta tag for image preview
     custom_meta_image_url: Optional[str] = None
+    # Optional custom meta url
+    custom_url: Optional[str] = None
     # Optional custom build directory for the frontend
     custom_build: Optional[str] = None
 
