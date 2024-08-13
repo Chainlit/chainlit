168c5a676a77a41235f4be7792bb0b357359b475
Willy Douhard <willy.douhard@gmail.com>
2024-08-13 13:45:23 +0200

    fix: lc callback (#1203)
    * fix: lc callback
    
    * fix: cors issue

--- backend/chainlit/server.py
+++ backend/chainlit/server.py
@@ -172,14 +172,17 @@ copilot_build_dir = get_build_dir(os.path.join("libs", "copilot"), "copilot")
 app = FastAPI(lifespan=lifespan)
 
 sio = socketio.AsyncServer(
-    cors_allowed_origins=[] if IS_SUBMOUNT else "*", async_mode="asgi"
+    cors_allowed_origins=[], async_mode="asgi"
 )
 
-combined_asgi_app = socketio.ASGIApp(
-    sio,
-    app,
-    socketio_path=f"{ROOT_PATH}/ws/socket.io" if ROOT_PATH else "/ws/socket.io",
+sio_mount_location = f"{ROOT_PATH}/ws" if ROOT_PATH else "ws"
+
+asgi_app = socketio.ASGIApp(
+    socketio_server=sio,
+    socketio_path=f"{sio_mount_location}/socket.io",
 )
+
+app.mount(f"/{sio_mount_location}", asgi_app)
 
 app.add_middleware(
     CORSMiddleware,
