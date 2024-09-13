3f1332479360b88ae4c184788f41f56743854f07
Matt Gartman <mattgartman@gmail.com>
2024-09-12 20:55:35 -0400

    add test for sendCookies

--- cypress/e2e/copilot/spec.cy.ts
+++ cypress/e2e/copilot/spec.cy.ts
@@ -1,5 +1,27 @@
 import { runTestServer, submitMessageCopilot } from '../../support/testUtils';
 
+function testCopilot() {
+  const opts = { includeShadowDom: true };
+  cy.get('#chainlit-copilot-button', opts).click();
+  cy.get('#chainlit-copilot-popover', opts).should('be.visible');
+
+  cy.get('#chainlit-copilot-popover', opts).within(() => {
+    cy.get('.step', opts).should('have.length', 1);
+    cy.contains('.step', 'Hi from copilot!', opts).should('be.visible');
+  });
+
+  submitMessageCopilot('Call func!');
+  cy.get('#chainlit-copilot-popover', opts).within(() => {
+    cy.get('.step', opts).should('have.length', 5);
+    cy.contains('.step', 'Function called with: Call func!', opts).should(
+      'be.visible'
+    );
+    cy.contains('.step', 'System message received: Hello World!', opts).should(
+      'be.visible'
+    );
+  });
+}
+
 describe('Copilot', () => {
   before(() => {
     runTestServer();
@@ -35,26 +57,47 @@ describe('Copilot', () => {
   });
 
   it('should be able to embed the copilot', () => {
-    const opts = { includeShadowDom: true };
-    cy.get('#chainlit-copilot-button', opts).click();
-    cy.get('#chainlit-copilot-popover', opts).should('be.visible');
+    testCopilot();
+  });
+});
 
-    cy.get('#chainlit-copilot-popover', opts).within(() => {
-      cy.get('.step', opts).should('have.length', 1);
-      cy.contains('.step', 'Hi from copilot!', opts).should('be.visible');
+describe('Copilot with sendCookies', () => {
+  before(() => {
+    runTestServer();
+    cy.document().then((document) => {
+      document.body.innerHTML = '<div id="root"><h1>Copilot test!</h1></div>';
+      const script = document.createElement('script');
+      // need to match baseUrl to allow origin to be set properly for CORS
+      script.src = 'http://127.0.0.1:8000/copilot/index.js';
+      document.body.appendChild(script);
     });
 
-    submitMessageCopilot('Call func!');
-    cy.get('#chainlit-copilot-popover', opts).within(() => {
-      cy.get('.step', opts).should('have.length', 5);
-      cy.contains('.step', 'Function called with: Call func!', opts).should(
-        'be.visible'
-      );
-      cy.contains(
-        '.step',
-        'System message received: Hello World!',
-        opts
-      ).should('be.visible');
+    // Wait for the script to load and execute the initialization
+    cy.window().then((win) => {
+      cy.wait(1000).then(() => {
+        // @ts-expect-error is not a valid prop
+        win.mountChainlitWidget({
+          // need to match baseUrl to allow origin to be set properly for CORS
+          chainlitServer: 'http://127.0.0.1:8000'
+        });
+
+        win.addEventListener('chainlit-call-fn', (e) => {
+          // @ts-expect-error is not a valid prop
+          win.sendChainlitMessage({
+            type: 'system_message',
+            output: 'Hello World!'
+          });
+          // @ts-expect-error is not a valid prop
+          const { name, args, callback } = e.detail;
+          if (name === 'test') {
+            callback('Function called with: ' + args.msg);
+          }
+        });
+      });
     });
+  });
+
+  it('should be able to embed the copilot', () => {
+    testCopilot();
   });
 });
