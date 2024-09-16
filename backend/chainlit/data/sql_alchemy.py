000dec1a57f80812677c338973cc6c9f6863b5e0
Josh Hayes <35790761+hayescode@users.noreply.github.com>
2024-09-16 12:26:47 -0500

    fix mypy

--- backend/chainlit/data/sql_alchemy.py
+++ backend/chainlit/data/sql_alchemy.py
@@ -378,7 +378,7 @@ class SQLAlchemyDataLayer(BaseDataLayer):
             logger.info(f"SQLAlchemy: get_element, thread_id={thread_id}, element_id={element_id}")
         query = """SELECT * FROM elements WHERE "threadId" = :thread_id AND "id" = :element_id"""
         parameters = {"thread_id": thread_id, "element_id": element_id}
-        element = await self.execute_sql(query=query, parameters=parameters)
+        element: List[dict[str, Any]] = await self.execute_sql(query=query, parameters=parameters)
         if element:
             return ElementDict(**element[0])
         else:
