1964409417ef5698237e5c03a104a847a92fda92
Daniel Avdar <66269169+DanielAvdar@users.noreply.github.com>
2024-09-20 20:13:18 +0300

    Get rid of context from SQL Alchemy data layer (#1319), fix SQLite support (#1137).
    * Add SQLite DB tests and fixtures
    * Get rid of context in SQL Alchemy data layer.
    
    ---------
    
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
    Co-authored-by: Mathijs de Bruin <mathijs@mathijsfietst.nl>

--- backend/tests/data/test_sql_alchemy.py
+++ backend/tests/data/test_sql_alchemy.py
@@ -1,14 +1,17 @@
+from unittest.mock import Mock
 import uuid
 from pathlib import Path
 
 import pytest
-
+import pytest_asyncio
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy import text
 
-from chainlit.data.base import BaseStorageClient
+from chainlit.data.base import BaseDataLayer, BaseStorageClient
 from chainlit.data.sql_alchemy import SQLAlchemyDataLayer
 from chainlit.element import Text
+from chainlit import User
+from chainlit.user import PersistedUser
 
 
 @pytest.fixture
@@ -113,7 +116,11 @@ async def data_layer(mock_storage_client: BaseStorageClient, tmp_path: Path):
     yield data_layer
 
 
-@pytest.mark.asyncio
+@pytest.fixture
+def test_user() -> User:
+    return User(identifier="sqlalchemy_test_user_id")
+
+
 async def test_create_and_get_element(
     mock_chainlit_context, data_layer: SQLAlchemyDataLayer
 ):
@@ -126,13 +133,84 @@ async def test_create_and_get_element(
             for_id="test_step_id",
         )
 
+        # Needs context because of wrapper in utils.py
         await data_layer.create_element(text_element)
 
-        retrieved_element = await data_layer.get_element(
-            text_element.thread_id, text_element.id
-        )
-        assert retrieved_element is not None
-        assert retrieved_element["id"] == text_element.id
-        assert retrieved_element["name"] == text_element.name
-        assert retrieved_element["mime"] == text_element.mime
-        # The 'content' field is not part of the ElementDict, so we remove this assertion
+    retrieved_element = await data_layer.get_element(
+        text_element.thread_id, text_element.id
+    )
+    assert retrieved_element is not None
+    assert retrieved_element["id"] == text_element.id
+    assert retrieved_element["name"] == text_element.name
+    assert retrieved_element["mime"] == text_element.mime
+    # The 'content' field is not part of the ElementDict, so we remove this assertion
+
+
+async def test_get_current_timestamp(data_layer: SQLAlchemyDataLayer):
+    timestamp = await data_layer.get_current_timestamp()
+    assert isinstance(timestamp, str)
+
+
+async def test_get_user(test_user: User, data_layer: SQLAlchemyDataLayer):
+    persisted_user = await data_layer.create_user(test_user)
+    assert persisted_user
+
+    fetched_user = await data_layer.get_user(persisted_user.identifier)
+
+    assert fetched_user
+    assert fetched_user.createdAt == persisted_user.createdAt
+    assert fetched_user.id == persisted_user.id
+
+    nonexistent_user = await data_layer.get_user("nonexistent")
+    assert nonexistent_user is None
+
+
+async def test_create_user(test_user: User, data_layer: SQLAlchemyDataLayer):
+    persisted_user = await data_layer.create_user(test_user)
+
+    assert persisted_user
+    assert persisted_user.identifier == test_user.identifier
+    assert persisted_user.createdAt
+    assert persisted_user.id
+
+    # Assert id is valid uuid
+    assert uuid.UUID(persisted_user.id)
+
+
+async def test_update_thread(test_user: User, data_layer: SQLAlchemyDataLayer):
+    persisted_user = await data_layer.create_user(test_user)
+    assert persisted_user
+
+    await data_layer.update_thread("test_thread")
+
+
+async def test_get_thread_author(test_user: User, data_layer: SQLAlchemyDataLayer):
+    persisted_user = await data_layer.create_user(test_user)
+    assert persisted_user
+
+    await data_layer.update_thread("test_thread", user_id=persisted_user.id)
+    author = await data_layer.get_thread_author("test_thread")
+
+    assert author == persisted_user.identifier
+
+
+async def test_get_thread(test_user: User, data_layer: SQLAlchemyDataLayer):
+    persisted_user = await data_layer.create_user(test_user)
+    assert persisted_user
+
+    await data_layer.update_thread("test_thread")
+    result = await data_layer.get_thread("test_thread")
+    assert result is not None
+
+    result = await data_layer.get_thread("nonexisting_thread")
+    assert result is None
+
+
+async def test_delete_thread(test_user: User, data_layer: SQLAlchemyDataLayer):
+    persisted_user = await data_layer.create_user(test_user)
+    assert persisted_user
+
+    await data_layer.update_thread("test_thread", "test_user")
+    await data_layer.delete_thread("test_thread")
+    thread = await data_layer.get_thread("test_thread")
+    assert thread is None
