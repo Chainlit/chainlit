e710bfbc070767662b230a718c37969809be3b52
jl1andricca <a.ricca@crif.com>
2024-09-21 11:48:13 +0200

    Factor storage clients into separate modules. Closes #1355.
    * Refactor storage_clients.py from a script to a module inside chainlit.data
    * Add pytest on S3StorageClient + add moto in tests dependencies
    * Moved BaseStorageClient from from chainlit.data.base.py to chainlit.data.storage_clients.base.py

---
+++ backend/chainlit/data/storage_clients/s3.py
@@ -0,0 +1,36 @@
+from typing import Any, Dict, Union
+
+import boto3  # type: ignore
+from chainlit.data.storage_clients.base import BaseStorageClient
+from chainlit.logger import logger
+
+
+class S3StorageClient(BaseStorageClient):
+    """
+    Class to enable Amazon S3 storage provider
+    """
+
+    def __init__(self, bucket: str):
+        try:
+            self.bucket = bucket
+            self.client = boto3.client("s3")
+            logger.info("S3StorageClient initialized")
+        except Exception as e:
+            logger.warn(f"S3StorageClient initialization error: {e}")
+
+    async def upload_file(
+        self,
+        object_key: str,
+        data: Union[bytes, str],
+        mime: str = "application/octet-stream",
+        overwrite: bool = True,
+    ) -> Dict[str, Any]:
+        try:
+            self.client.put_object(
+                Bucket=self.bucket, Key=object_key, Body=data, ContentType=mime
+            )
+            url = f"https://{self.bucket}.s3.amazonaws.com/{object_key}"
+            return {"object_key": object_key, "url": url}
+        except Exception as e:
+            logger.warn(f"S3StorageClient, upload_file error: {e}")
+            return {}
