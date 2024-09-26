c9aaa4b9d247a4435056495ae1d86ea6f565f180
EWouters <6179932+EWouters@users.noreply.github.com>
2024-09-26 20:06:04 +0200

    Fixed error message

--- backend/chainlit/discord/__init__.py
+++ backend/chainlit/discord/__init__.py
@@ -2,5 +2,5 @@ import importlib.util
 
 if importlib.util.find_spec("discord") is None:
     raise ValueError(
-        "The discord package is required to integrate Chainlit with a Slack app. Run `pip install discord --upgrade`"
+        "The discord package is required to integrate Chainlit with a Discord app. Run `pip install discord --upgrade`"
     )
