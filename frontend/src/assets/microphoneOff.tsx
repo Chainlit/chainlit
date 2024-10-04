8882619742b8596cc016be084a51049fd1687f8f
Willy Douhard <willy.douhard@gmail.com>
2024-10-04 18:22:53 +0200

    Willy/realtime (#1401)
    * Bump literalai depdendency.
    
    * Update literalai imports
    
    * Remove unused imports from SQLAlchemy tests.
    
    * Unit tests for LiteralDataLayer.
    
    * Consistent LiteralAI to Chainlit conversion, resolve PaginatedResponse exceptions.
    
    - Create LiteralToChainlitConverter class for handling conversions
    - Implement methods for converting steps, threads, and attachments
    - Add support for different Element subclasses based on metadata
    - Allow manual setting of thread_id and id for Step and Element
    
    * Attempt to satisfy mypy (plus cleaner approach).
    
    * feat: add realtime audio
    
    * fix: default config
    
    * fix: lint
    
    ---------
    
    Co-authored-by: Mathijs de Bruin <mathijs@mathijsfietst.nl>
    Co-authored-by: EWouters <6179932+EWouters@users.noreply.github.com>

--- frontend/src/assets/microphoneOff.tsx
+++ frontend/src/assets/microphoneOff.tsx
@@ -1,6 +1,6 @@
 import SvgIcon, { SvgIconProps } from '@mui/material/SvgIcon';
 
-const MicrophoneIcon = (props: SvgIconProps) => {
+const MicrophoneOffIcon = (props: SvgIconProps) => {
   return (
     <SvgIcon
       {...props}
@@ -13,11 +13,14 @@ const MicrophoneIcon = (props: SvgIconProps) => {
       }}
       viewBox="0 0 24 24"
     >
-      <path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z" />
-      <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
+      <line x1="2" x2="22" y1="2" y2="22" />
+      <path d="M18.89 13.23A7.12 7.12 0 0 0 19 12v-2" />
+      <path d="M5 10v2a7 7 0 0 0 12 5" />
+      <path d="M15 9.34V5a3 3 0 0 0-5.68-1.33" />
+      <path d="M9 9v3a3 3 0 0 0 5.12 2.12" />
       <line x1="12" x2="12" y1="19" y2="22" />
     </SvgIcon>
   );
 };
 
-export default MicrophoneIcon;
+export default MicrophoneOffIcon;
