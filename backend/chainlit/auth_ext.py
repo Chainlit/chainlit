fa11f719a5633a01ff9c3ae94be5e2c2bf3da1d6
Loh Wei Jun <wjloh91@gmail.com>
2024-10-09 16:46:33 +0800

    chore: Update TTLCache key value types to be both string

--- backend/chainlit/auth_ext.py
+++ backend/chainlit/auth_ext.py
@@ -2,6 +2,6 @@ from cachetools import TTLCache
 from chainlit.config import config
 
 
-jwt_session_tokens: TTLCache[int, int] = TTLCache(
+jwt_session_tokens: TTLCache[str, str] = TTLCache(
     maxsize=8096, ttl=config.project.user_session_timeout
 )
