7cd89bb01538ee1d16b0573f092d269e68f272bd
Loh Wei Jun <wjloh91@gmail.com>
2024-10-10 02:04:14 +0800

    chore: Remove Type subscript for TLLCache due to ABCMeta issue in python 3.8 and below

--- backend/chainlit/auth_ext.py
+++ backend/chainlit/auth_ext.py
@@ -1,7 +1,9 @@
+from typing import Any
+
 from cachetools import TTLCache
+
 from chainlit.config import config
 
-
-jwt_session_tokens: TTLCache[str, str] = TTLCache(
+jwt_session_tokens: TTLCache = TTLCache(
     maxsize=8096, ttl=config.project.user_session_timeout
 )
