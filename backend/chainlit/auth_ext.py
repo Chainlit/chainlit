122f036d2d8b089b8696c7270b165af30b496dd0
Loh Wei Jun <wjloh91@gmail.com>
2024-10-09 16:35:51 +0800

    chore: Add types to jwt_session_tokens

--- backend/chainlit/auth_ext.py
+++ backend/chainlit/auth_ext.py
@@ -1,7 +1,7 @@
-import cachetools
+from cachetools import TTLCache
 from chainlit.config import config
 
 
-jwt_session_tokens = cachetools.TTLCache(
+jwt_session_tokens: TTLCache[int, int] = TTLCache(
     maxsize=8096, ttl=config.project.user_session_timeout
 )
