beb44ca74f18b352fa078baf7038c2dc6b729e0c
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-10-17 09:19:15 +0100

    Assorted backend fixes/cleanup (#1432)
    * Suppress: Possible hardcoded password (bandit B105)
    * Fix: Redefinition of unused `on_action` (Ruff F811)
    * Use `importlib.util.find_spec` to check if a package is installed
    * Raise from Exception
    * Fixed undefined variable
    * Fixed error message
    
    ---------
    
    Authored-by: EWouters <6179932+EWouters@users.noreply.github.com>

--- backend/chainlit/teams/__init__.py
+++ backend/chainlit/teams/__init__.py
@@ -1,6 +1,6 @@
-try:
-    import botbuilder
-except ModuleNotFoundError:
+import importlib.util
+
+if importlib.util.find_spec("botbuilder") is None:
     raise ValueError(
         "The botbuilder-core package is required to integrate Chainlit with a Slack app. Run `pip install botbuilder-core --upgrade`"
     )
