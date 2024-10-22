060b9fd635f84014e14bd0748bbbebbe4cf32aea
jfperusse-bhvr <Jean-Francois.Perusse@bhvr.com>
2024-10-22 16:16:38 -0400

    Add missing import of send_window_message

--- backend/chainlit/__init__.py
+++ backend/chainlit/__init__.py
@@ -44,7 +44,7 @@ from chainlit.message import (
 )
 from chainlit.step import Step, step
 from chainlit.sync import make_async, run_sync
-from chainlit.types import InputAudioChunk, OutputAudioChunk, ChatProfile, Starter
+from chainlit.types import ChatProfile, InputAudioChunk, OutputAudioChunk, Starter
 from chainlit.user import PersistedUser, User
 from chainlit.user_session import user_session
 from chainlit.utils import make_module_getattr
@@ -57,18 +57,19 @@ from .callbacks import (
     author_rename,
     header_auth_callback,
     oauth_callback,
-    on_audio_start,
     on_audio_chunk,
     on_audio_end,
+    on_audio_start,
     on_chat_end,
     on_chat_resume,
     on_chat_start,
     on_logout,
     on_message,
-    on_window_message,
     on_settings_update,
     on_stop,
+    on_window_message,
     password_auth_callback,
+    send_window_message,
     set_chat_profiles,
     set_starters,
 )
