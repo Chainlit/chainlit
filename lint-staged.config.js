f47436d2b991aff57176bbdb96f4b0ec468f6a72
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-10-31 10:32:14 +0000

    Pre-commit/CI and build improvements.
    * Small improvements to build script.
    * Use ruff for linting and formatting.
    * Use mypy daemon for (much) faster type-checking.
    * Add typing to `cache.py` to fix it's requirements.
    * Bump to latest mypy for increased performance.
    * Bump husky to latest version.
    * Only lint Python when Python files have changed.
    * Linting for GitHub Actions.
      Requires actionlint: https://github.com/rhysd/actionlint
    * Linting and formatting check in CI.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  "**/*.{js,jsx,ts,tsx}": ["npx prettier --write", "npx eslint --fix"],
-  "**/*.{ts,tsx}": [() => "tsc --skipLibCheck --noEmit"],
-  "**/*.py": ["black", "isort --profile=black"],
+  '**/*.{js,jsx,ts,tsx}': ['npx prettier --write', 'npx eslint --fix'],
+  '**/*.{ts,tsx}': [() => 'tsc --skipLibCheck --noEmit'],
+  '**/*.py': ['ruff check --fix', 'ruff format', () => 'pnpm run lintPython'],
+  '.github/{workflows,actions}/**': ['actionlint']
 };
