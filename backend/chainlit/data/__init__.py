6d7436bcf7b87ad4ded5c26a5a7073181404285a
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-11-06 14:29:11 +0000

    Remove leftover debug print statement.

--- backend/chainlit/data/__init__.py
+++ backend/chainlit/data/__init__.py
@@ -17,7 +17,6 @@ def get_data_layer():
         from chainlit.config import config
 
         if config.code.data_layer:
-            print("Getting data layer from config")
             # When @data_layer is configured, call it to get data layer.
             _data_layer = config.code.data_layer()
         elif api_key := os.environ.get("LITERAL_API_KEY"):
