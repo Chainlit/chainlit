b599372f71830ae39fdcad9dc0bcff0f5111fa7b
Willy Douhard <willy.douhard@gmail.com>
2024-12-09 15:08:56 +0100

    fix: ci

--- backend/chainlit/server.py
+++ backend/chainlit/server.py
@@ -187,10 +187,7 @@ copilot_build_dir = get_build_dir(os.path.join("libs", "copilot"), "copilot")
 
 app = FastAPI(lifespan=lifespan)
 
-sio = socketio.AsyncServer(
-    cors_allowed_origins=[],
-    async_mode="asgi"
-)
+sio = socketio.AsyncServer(cors_allowed_origins=[], async_mode="asgi")
 
 asgi_app = socketio.ASGIApp(
     socketio_server=sio,
