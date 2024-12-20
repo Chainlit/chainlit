b561b4b453e8bf7aca71ffbc9ba8656494d546fd
Hugues de Saxcé <hugues.de.saxce@protonmail.com>
2024-12-20 11:21:21 +0100

    fix: gcs

--- backend/chainlit/data/storage_clients/gcs.py
+++ backend/chainlit/data/storage_clients/gcs.py
@@ -4,11 +4,14 @@ from typing import Any, Dict, Union
 from google.cloud import storage  # type: ignore
 from google.oauth2 import service_account
 
+from chainlit import logger
 from chainlit.data.storage_clients.base import BaseStorageClient
 
 
 class GCSStorageClient(BaseStorageClient):
-    def __init__(self, project_id: str, client_email: str, private_key: str):
+    def __init__(
+        self, project_id: str, client_email: str, private_key: str, bucket_name: str
+    ):
         private_key = base64.b64decode(private_key).decode("utf-8")
 
         credentials = service_account.Credentials.from_service_account_info(
@@ -22,6 +25,9 @@ class GCSStorageClient(BaseStorageClient):
         )
 
         self.client = storage.Client(project=project_id, credentials=credentials)
+        self.bucket_name = bucket_name
+        self.bucket = self.client.bucket(bucket_name)
+        logger.info("GCSStorageClient initialized")
 
     async def upload_file(
         self,
@@ -31,11 +37,7 @@ class GCSStorageClient(BaseStorageClient):
         overwrite: bool = True,
     ) -> Dict[str, Any]:
         try:
-            bucket_name, *blob_parts = object_key.split("/")
-            blob_path = "/".join(blob_parts)
-
-            bucket = self.client.bucket(bucket_name)
-            blob = bucket.blob(blob_path)
+            blob = self.bucket.blob(object_key)
 
             if not overwrite and blob.exists():
                 raise Exception(
@@ -48,12 +50,8 @@ class GCSStorageClient(BaseStorageClient):
             blob.upload_from_string(data, content_type=mime)
 
             return {
-                "bucket": bucket_name,
-                "name": blob_path,
-                "size": len(data),
-                "contentType": mime,
-                "url": f"gs://{bucket_name}/{blob_path}",
-                "metadata": blob.metadata,
+                "object_key": object_key,
+                "url": f"gs://{self.bucket_name}/{object_key}",
             }
 
         except Exception as e:
