e62092f01874fd07389b2d6c411a495d3f82384a
Willy Douhard <willy.douhard@gmail.com>
2025-01-03 17:25:10 +0100

    feat: add autofocus on login input

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -102,6 +102,7 @@ export function LoginForm({
               </Label>
               <Input
                 id="email"
+                autoFocus
                 placeholder="me@example.com"
                 {...register('email', {
                   required: t(
