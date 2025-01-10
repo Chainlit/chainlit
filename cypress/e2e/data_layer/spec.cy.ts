0758dd5da966496fa649882ea70ba8d87b711148
Willy Douhard <willy.douhard@gmail.com>
2025-01-10 14:10:39 +0100

    fix: ci

--- cypress/e2e/data_layer/spec.cy.ts
+++ cypress/e2e/data_layer/spec.cy.ts
@@ -18,11 +18,8 @@ function feedback() {
     expect(loc).to.match(/^\/thread\//);
   });
   cy.get('.negative-feedback-off').should('have.length', 1);
-  cy.get('.positive-feedback-off')
-    .should('have.length', 1)
-    .eq(0)
-    .click({ force: true });
-  cy.get('#submit-feedback').click({ force: true });
+  cy.get('.positive-feedback-off').should('have.length', 1).eq(0).click();
+  cy.get('#submit-feedback').click();
   cy.get('.positive-feedback-on').should('have.length', 1);
 }
 
@@ -37,27 +34,28 @@ function threadList() {
   cy.get('#thread-test2').should('contain', 'thread 2');
 
   // Test thread page
-  cy.get('#thread-test1').click({ force: true });
+  cy.get('#thread-test1').click();
   cy.get('.step').should('have.length', 2);
   cy.get('.step').eq(0).should('contain', 'Message 1');
   cy.get('.step').eq(1).should('contain', 'Message 2');
 
   // Test thread delete
-  cy.get('#thread-test1').find('#thread-options').click({ force: true });
+  cy.get('#thread-test1').find('#thread-options').click();
   cy.wait(100);
-  cy.get('#delete-thread').click({ force: true });
+  cy.get('#delete-thread').click();
   cy.wait(100);
-  cy.get("[type='button']").contains('Confirm').click({ force: true });
+  cy.get("[type='button']").contains('Confirm').click();
   cy.wait(100);
   cy.get('#thread-test1').should('not.exist');
 }
 
 function resumeThread() {
+  cy.wait(1000);
   // Go to the "thread 2" thread and resume it
-  cy.get('#thread-test2').click({ force: true });
+  cy.get('#thread-test2').click();
   cy.wait(1000);
   // back to the "hello" thread
-  cy.get('a').contains('Hello').click({ force: true });
+  cy.get('a').contains('Hello').click();
   cy.wait(1000);
 
   cy.get('.step').should('have.length', 10);
@@ -89,8 +87,8 @@ function continueThread() {
 }
 
 function newThread() {
-  cy.get('#new-chat-button').click({ force: true });
-  cy.get('#confirm').click({ force: true });
+  cy.get('#new-chat-button').click();
+  cy.get('#confirm').click();
 }
 
 describe('Data Layer', () => {
