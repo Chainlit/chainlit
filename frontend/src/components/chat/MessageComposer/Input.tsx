65720fdcd463c9d9c9fc143cb778591dcf7e0f3d
Lionel Lamy <contact@llamy.be>
2025-02-05 14:36:44 +0100

    Empty placeholder

--- frontend/src/components/chat/MessageComposer/Input.tsx
+++ frontend/src/components/chat/MessageComposer/Input.tsx
@@ -352,7 +352,7 @@ const Input = forwardRef<InputMethods, Props>(
           autoFocus={autoFocus}
           ref={contentEditableRef}
           contentEditable={!disabled}
-          data-placeholder={placeholder}
+          data-placeholder={disabled ? '' : placeholder}
           className={cn(
             'min-h-10 max-h-[250px] overflow-y-auto w-full focus:outline-none focus:ring-0 focus:ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0 empty:before:content-[attr(data-placeholder)] empty:before:text-muted-foreground',
             className
