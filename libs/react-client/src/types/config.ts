38a6f32b2d24c1b3158064ecd81bb0d779145f29
Willy Douhard <willy.douhard@gmail.com>
2025-02-08 18:03:17 -0800

    feat: add default_open to step class (#1879)
    * feat: add default_open to step class
    
    * fix: ruff

--- libs/react-client/src/types/config.ts
+++ libs/react-client/src/types/config.ts
@@ -39,7 +39,7 @@ export interface IChainlitConfig {
     custom_js?: string;
     custom_font?: string;
     custom_meta_image_url?: string;
-    header_links?: { name: string; icon_url: string, url: string }[];
+    header_links?: { name: string; icon_url: string; url: string }[];
   };
   features: {
     spontaneous_file_upload?: {
