1057ab5e5e967c1f9b0984e859ab558f317c6cba
Joseph Hau <hexart@gmail.com>
2025-02-16 14:42:06 +0800

    Add clipPath to ensure image blur doesn’t overflow

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -113,7 +113,8 @@ export default function Login() {
             }
             alt="Image"
             className="absolute inset-0 h-full w-full object-cover"
-            style={{ filter: filterStyle }}
+            style={{ filter: filterStyle }},
+            clipPath: 'inset(0)'
           />
         </div>
       ) : null}
