5f38f8570a009bf7094a2eb33630b9068812069f
eavanvalkenburg <github@vanvalkenburg.eu>
2025-03-12 10:04:36 +0100

    updated filter, added to init

--- backend/chainlit/semantic_kernel/__init__.py
+++ backend/chainlit/semantic_kernel/__init__.py
@@ -1,7 +1,8 @@
 from collections.abc import Awaitable, Callable
-from typing import Any, TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
+
 from pydantic import BaseModel
-from semantic_kernel.filters import FilterTypes
+
 from chainlit import Step
 
 if TYPE_CHECKING:
@@ -9,17 +10,19 @@ if TYPE_CHECKING:
     from semantic_kernel.filters import FunctionInvocationContext
     from semantic_kernel.functions import KernelArguments
 
+
 class SemanticKernelFilter(BaseModel):
     """Semantic Kernel Filter for Chainlit.
 
     This filter wraps any function calls that are executed and will capture the input and output of that function
-    as a Chainlit step.
+    as a Chainlit Step.
 
-    After creating the filter, just call `.add_to_kernel` method with your kernel.
+    You can pass your kernel into the constructor, or you can call `add_to_kernel` later.
 
     Args:
         excluded_plugins: a list of plugin_names that will be excluded from displaying steps.
         excluded_functions: a list of function names that will be excluded from displaying steps.
+        kernel: the Kernel to add the filter to. If not provided, you can call `add_to_kernel` later.
 
     Methods:
         add_to_kernel: this method takes a Kernel and adds the filter to that kernel.
@@ -28,53 +31,66 @@ class SemanticKernelFilter(BaseModel):
 
     Example::
 
+        filter = SemanticKernelFilter(kernel=kernel)
+
+        # or when you create your kernel later on:
+
         filter = SemanticKernelFilter()
+        # ...
+        # other code, including kernel creation.
+        # ...
         filter.add_to_kernel(kernel)
     """
 
     excluded_plugins: list[str] | None = None
     excluded_functions: list[str] | None = None
 
-    def add_to_kernel(self, kernel: 'Kernel') -> None:
+    def __init__(
+        self,
+        excluded_plugins: list[str] | None = None,
+        excluded_functions: list[str] | None = None,
+        *,
+        kernel: "Kernel | None" = None,
+    ) -> None:
+        super().__init__(
+            excluded_plugins=excluded_plugins, excluded_functions=excluded_functions
+        )
+        if kernel:
+            self.add_to_kernel(kernel)
+
+    def add_to_kernel(self, kernel: "Kernel") -> None:
         """Adds the filter to the provided kernel.
 
         Args:
             kernel: the Kernel to add the filter to.
         """
-        kernel.add_filter(FilterTypes.FUNCTION_INVOCATION, self._function_invocation_filter)
+        kernel.add_filter("function_invocation", self._function_invocation_filter)
 
     def parse_arguments(self, arguments: "KernelArguments") -> dict[str, Any] | str:
         """Parse the KernelArguments used for the function.
-        
+
         This function can be subclassed to easily adopt how the input arguments are displayed.
 
         Args:
             arguments: KernelArguments
 
         Returns:
-            a dict or string with the input.        
+            a dict or string with the input.
         """
         if len(arguments) == 0:
-            return ''
+            return ""
         input_dict = {}
         for key, value in arguments.items():
             if isinstance(value, BaseModel):
-                if key == "ChatHistory":
-                    input_dict[key] = value.model_dump_json(
-                        indent=4, exclude_none=True, by_alias=True
-                    )
-                else:
-                    input_dict[key] = value.model_dump(
-                        exclude_none=True, by_alias=True
-                    )
+                input_dict[key] = value.model_dump(exclude_none=True, by_alias=True)
             else:
                 input_dict[key] = value
         return input_dict
 
     async def _function_invocation_filter(
         self,
-        context: 'FunctionInvocationContext',
-        next: Callable[['FunctionInvocationContext'], Awaitable[None]],
+        context: "FunctionInvocationContext",
+        next: Callable[["FunctionInvocationContext"], Awaitable[None]],
     ):
         if (
             self.excluded_plugins
