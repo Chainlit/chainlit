17b6446beecf5bf49cf03aa215e3e0ec49809caf
Josh Hayes <35790761+hayescode@users.noreply.github.com>
2025-03-25 20:40:22 -0500

    Update ButtonLink.tsx

--- frontend/src/components/ButtonLink.tsx
+++ frontend/src/components/ButtonLink.tsx
@@ -24,23 +24,19 @@ export default function ButtonLink({ name, iconUrl, url }: ButtonLinkProps) {
         <TooltipTrigger asChild>
           <Button
             variant="ghost"
-            size="icon"
-            className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
+            className="text-muted-foreground hover:text-muted-foreground"
           >
-            <a href={url} target="_blank">
-              {iconUrl ? (
-                <img
-                  src={
-                    iconUrl.startsWith('/public')
-                      ? apiClient.buildEndpoint(iconUrl)
-                      : iconUrl
-                  }
-                  className={'h-6 w-6'}
-                  alt={name}
-                />
-              ) : (
-                <span>{name}</span>
-              )}
+            <a href={url} target="_blank" className="inline-flex items-center gap-1">
+              <img
+                src={
+                  iconUrl?.startsWith('/public')
+                    ? apiClient.buildEndpoint(iconUrl)
+                    : iconUrl
+                }
+                className={'h-6 w-6'}
+                alt={name}
+              />
+              {name}
             </a>
           </Button>
         </TooltipTrigger>
