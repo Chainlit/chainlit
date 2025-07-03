a5158da71ee66a408ec2f77da238dab0e51e2541
Lao <laodanfeng@hotmail.com>
2025-07-03 16:04:59 +0800

    Update dropdown-menu.tsx
    fix(ui): adding container prop to dropdown portal (Render the Portal Inside Copilot's Shadow DOM)

--- frontend/src/components/ui/dropdown-menu.tsx
+++ frontend/src/components/ui/dropdown-menu.tsx
@@ -57,7 +57,7 @@ const DropdownMenuContent = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
 >(({ className, sideOffset = 4, ...props }, ref) => (
-  <DropdownMenuPrimitive.Portal>
+  <DropdownMenuPrimitive.Portal container={window.cl_shadowRootElement || document.body}>
     <DropdownMenuPrimitive.Content
       ref={ref}
       sideOffset={sideOffset}
