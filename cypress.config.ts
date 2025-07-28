87c9a64dc15d548e220576fd95043373ba44be44
Aleksandr Vishniakov <asvishnyakov@hotmail.com>
2025-07-28 21:01:20 +0200

    test: fix server shutdown in edge cases

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,23 @@
 import { ChildProcessWithoutNullStreams } from 'child_process';
 import { defineConfig } from 'cypress';
+import fkill from 'fkill';
 
 import { runChainlit } from './cypress/support/run';
 
+export const CHAINLIT_APP_PORT = 8000;
+
 let chainlit: ChildProcessWithoutNullStreams;
+
+['SIGTERM', 'SIGINT', 'SIGHUP', 'SIGBREAK'].forEach((signal) => {
+  process.on(signal, () => {
+    if (chainlit) {
+      chainlit.kill();
+    }
+
+    const signalMap = { SIGTERM: 15, SIGINT: 2, SIGHUP: 1, SIGBREAK: 21 };
+    process.exit(128 + (signalMap[signal] || 0));
+  });
+});
 
 export default defineConfig({
   projectId: 'ij1tyk',
@@ -21,8 +35,10 @@ export default defineConfig({
 
   e2e: {
     defaultCommandTimeout: 30000,
-    baseUrl: 'http://127.0.0.1:8000',
+    baseUrl: `http://127.0.0.1:${CHAINLIT_APP_PORT}`,
     async setupNodeEvents(on, config) {
+      await fkill(`:${CHAINLIT_APP_PORT}`, { silent: true });
+
       chainlit = await runChainlit();
 
       on('before:spec', async (spec) => {
