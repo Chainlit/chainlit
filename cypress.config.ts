888a6be4b138c675643c3d67e10dd004f69cca87
Aleksandr Vishniakov <asvishnyakov@hotmail.com>
2025-07-28 21:55:53 +0200

    test: attempt to fix process killing on windows

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { ChildProcessWithoutNullStreams } from 'child_process';
 import { defineConfig } from 'cypress';
 import fkill from 'fkill';
+import { platform } from 'os';
 
 import { runChainlit } from './cypress/support/run';
 
@@ -8,10 +9,21 @@ export const CHAINLIT_APP_PORT = 8000;
 
 let chainlit: ChildProcessWithoutNullStreams;
 
+function killChainlit() {
+  if (chainlit) {
+    if (platform() === 'win32') {
+      chainlit.stdin.destroy();
+      chainlit.stdout.destroy();
+      chainlit.stderr.destroy();
+    }
+    chainlit.kill();
+  }
+}
+
 ['SIGTERM', 'SIGINT', 'SIGHUP', 'SIGBREAK'].forEach((signal) => {
   process.on(signal, () => {
     if (chainlit) {
-      chainlit.kill();
+      killChainlit(); // Ensure Chainlit is killed on exit
     }
 
     const signalMap = { SIGTERM: 15, SIGINT: 2, SIGHUP: 1, SIGBREAK: 21 };
@@ -37,26 +49,26 @@ export default defineConfig({
     defaultCommandTimeout: 30000,
     baseUrl: `http://127.0.0.1:${CHAINLIT_APP_PORT}`,
     async setupNodeEvents(on, config) {
-      await fkill(`:${CHAINLIT_APP_PORT}`, { silent: true });
+      await fkill(`:${CHAINLIT_APP_PORT}`, { silent: true }); // Fallback to ensure no previous instance is running
 
       chainlit = await runChainlit();
 
       on('before:spec', async (spec) => {
         if (chainlit) {
-          chainlit.kill();
+          killChainlit();
         }
         chainlit = await runChainlit(spec);
       });
 
       on('after:spec', () => {
         if (chainlit) {
-          chainlit.kill();
+          killChainlit();
         }
       });
 
       on('after:run', () => {
         if (chainlit) {
-          chainlit.kill();
+          killChainlit();
         }
       });
 
@@ -67,7 +79,7 @@ export default defineConfig({
         },
         restartChainlit(spec: Cypress.Spec) {
           if (chainlit) {
-            chainlit.kill();
+            killChainlit();
           }
           return new Promise((resolve) => {
             runChainlit(spec).then((newChainlit) => {
