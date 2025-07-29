0458b80063082293085073f4b40059e2ac1dd4d5
dominnpm <dompanmol@alum.us.es>
2025-07-29 09:47:09 +0200

    refactor : env name

--- backend/chainlit/auth/cookie.py
+++ backend/chainlit/auth/cookie.py
@@ -26,7 +26,7 @@ _cookie_secure = _cookie_samesite == "none"
 if _cookie_root_path := os.environ.get("CHAINLIT_ROOT_PATH", None):
     _cookie_path = os.environ.get(_cookie_root_path, "/")
 else:
-    _cookie_path = os.environ.get("CHAINLIT_COOKIE_PATH", "/")
+    _cookie_path = os.environ.get("CHAINLIT_AUTH_COOKIE_PATH", "/")
 _state_cookie_lifetime = 3 * 60  # 3m
 _auth_cookie_name = os.environ.get("CHAINLIT_AUTH_COOKIE_NAME", "access_token")
 _state_cookie_name = "oauth_state"
