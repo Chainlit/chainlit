331f004d1f3bd3cf650a30215b489d937cc829a9
Aleksandr Vishniakov <asvishnyakov@hotmail.com>
2025-08-01 05:29:08 +0200

    test: implement workaround for ResizeObserver

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,5 +1,20 @@
 import 'cypress-plugin-steps';
 
+/*
+ * This is a workaround for the ResizeObserver loop error that occurs in Cypress.
+ * See https://github.com/cypress-io/cypress/issues/20341
+ * See https://github.com/cypress-io/cypress/issues/29277
+ */
+Cypress.on('uncaught:exception', (err) => {
+  if (
+    err.message.includes(
+      'ResizeObserver loop completed with undelivered notifications'
+    )
+  ) {
+    return false;
+  }
+});
+
 beforeEach(() => {
   cy.visit('/');
 });
