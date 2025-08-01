772eeffb4df62d12b1cdf6d58674ec508842afcd
Aleksandr Vishniakov <asvishnyakov@hotmail.com>
2025-08-01 06:21:21 +0200

    ci: improve e2e tests stability & performance (#2327)
    * ci: retry individual tests
    
    * chore: upgrade cypress
    
    * refactor: server running
    
    Also tests fixed
    
    * test: open main page before each test, not spec
    
    * test: fix copilot retry
    
    * test: fix server shutdown in edge cases
    
    * chore: revert back some additional logging
    
    * test: attempt to fix process killing on windows
    
    * test: fix chainlit process killing
    
    * ci: attempt to fix windows workflow
    
    * ci: another attemp to fix Windows tests
    
    * ci: store screenshots, if produces
    
    * test: fix data_layer
    
    * chore: disable server run/kill logging
    
    * test: timeout after chainlit restart
    
    * ci: fix screenshots uploading
    
    * chore: bring back cypress interactive mode
    
    * test: add cypress steps plugin
    
    * test: fix data_layer
    
    * chore: add test:interactive command
    
    * test: fix chat settings
    
    * test: another attemp to fix chat_settings
    
    * test: improve chat_settings
    
    * test: try another approach with chat_settings
    
    * test: implement workaround for ResizeObserver
    
    * test: disable component config
    
    * test: add steps to copilot
    
    * docs: fix pnpm test folder in contributing guideline
    
    * docs: small explanation

--- cypress/e2e/chat_settings/spec.cy.ts
+++ cypress/e2e/chat_settings/spec.cy.ts
@@ -1,18 +1,18 @@
-import { runTestServer } from '../../support/testUtils';
+const openChatSettingsModal = () => {
+  cy.step('Open chat settings modal');
+
+  cy.get('#chat-settings-open-modal').should('exist').click();
+  cy.get('#chat-settings').should('exist').and('be.visible');
+};
 
 describe('Customize chat settings', () => {
-  before(() => {
-    runTestServer();
-  });
-
   it('should update inputs', () => {
-    // Open chat settings modal
-    cy.get('#chat-settings-open-modal').should('exist');
-    cy.get('#chat-settings-open-modal').click();
-    cy.get('#chat-settings').should('exist');
+    openChatSettingsModal();
 
-    // Update inputs
+    cy.step('Update inputs');
+
     cy.get('#Model').click();
+    cy.get('[role="listbox"]').should('be.visible');
     cy.contains('gpt-4').click();
     cy.get('#Model').should('contain.text', 'gpt-4');
 
@@ -28,7 +28,7 @@ describe('Customize chat settings', () => {
       .type('{rightarrow}'.repeat(6))
       .should('have.attr', 'aria-valuenow', '36');
 
-      cy.get('#SAI_Cfg_Scale')
+    cy.get('#SAI_Cfg_Scale')
       .find('[role="slider"]')
       .focus()
       .type('{rightarrow}'.repeat(6))
@@ -39,24 +39,34 @@ describe('Customize chat settings', () => {
     cy.get('.step').should('have.length', 1);
     cy.get('.step').eq(0).should('contain', 'Settings updated!');
 
-    // Check if inputs are updated
-    cy.get('#chat-settings-open-modal').click();
-    cy.get('#Temperature').find('[role="slider"]')
-    .should('have.attr', 'aria-valuenow', '0.4');
+    openChatSettingsModal();
 
-    cy.get('#SAI_Steps').find('[role="slider"]')
-    .should('have.attr', 'aria-valuenow', '36');
+    cy.step('Check inputs are updated');
 
-    cy.get('#SAI_Cfg_Scale').find('[role="slider"]')
-    .should('have.attr', 'aria-valuenow', '7.6');
+    cy.get('#Model').should('contain.text', 'gpt-4');
 
-    cy.get('#SAI_Width').find('[role="slider"]')
-    .should('have.attr', 'aria-valuenow', '512');
+    cy.get('#Temperature')
+      .find('[role="slider"]')
+      .should('have.attr', 'aria-valuenow', '0.4');
 
-    cy.get('#SAI_Height').find('[role="slider"]')
-    .should('have.attr', 'aria-valuenow', '512');
+    cy.get('#SAI_Steps')
+      .find('[role="slider"]')
+      .should('have.attr', 'aria-valuenow', '36');
 
-    // Check if modal is correctly closed
+    cy.get('#SAI_Cfg_Scale')
+      .find('[role="slider"]')
+      .should('have.attr', 'aria-valuenow', '7.6');
+
+    cy.get('#SAI_Width')
+      .find('[role="slider"]')
+      .should('have.attr', 'aria-valuenow', '512');
+
+    cy.get('#SAI_Height')
+      .find('[role="slider"]')
+      .should('have.attr', 'aria-valuenow', '512');
+
+    cy.step('Check if modal is correctly closed');
+
     cy.contains('Cancel').click();
     cy.get('#chat-settings').should('not.exist');
   });
