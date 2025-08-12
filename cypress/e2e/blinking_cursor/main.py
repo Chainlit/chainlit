dd4985c23c3fc7a03c0470aa6c8f8c2802e96456
jescudero <jescudero@itos.es>
2025-08-12 18:05:45 +0200

    test tool and message. use timeout instead of sleep

--- cypress/e2e/blinking_cursor/main.py
+++ cypress/e2e/blinking_cursor/main.py
@@ -6,7 +6,17 @@ async def main():
     await cl.Message("Hello, this is a test message!").send()
 
 
+@cl.step(type="tool")
+async def tool():
+    # Fake tool
+    await cl.sleep(5)
+    return "Response from the tool!"
+
+
 @cl.on_message
 async def on_message(message: cl.Message):
-    await cl.sleep(5)
-    await cl.Message(f"Received message: {message.content}").send()
+    if message.content == "tool":
+        await tool()
+    else:
+        await cl.sleep(5)
+        await cl.Message(f"Received message: {message.content}").send()
--- cypress/e2e/blinking_cursor/spec.cy.ts
+++ cypress/e2e/blinking_cursor/spec.cy.ts
@@ -1,20 +1,26 @@
 import { submitMessage } from '../../support/testUtils';
 
 describe('Blinking cursor', () => {
-  it('It should display until a message is sent', () => {
+  it('It should display until a step or message is sent', () => {
     cy.get('.step').should('have.length', 1);
     cy.contains('.step', 'Hello, this is a test message!').should('be.visible');
 
-    submitMessage('Jeeves');
+    submitMessage('tool');
 
-    cy.get('.step').should('have.length', 2);
-    cy.get('.step').last().should('contain.text', 'Jeeves');
-    cy.get('.step').last().next('.animate-pulse').should('exist');
-
-    cy.wait(5500);
-
-    cy.get('.step').last().next('.animate-pulse').should('not.exist');
     cy.get('.step').should('have.length', 3);
+    cy.get('.step').last().should('have.attr', 'data-step-type', 'tool');
+    cy.get('.step').last().next('.animate-pulse').should('not.exist');
+
+    submitMessage('Jeeves');
+    cy.get('.step')
+      .last()
+      .next('.animate-pulse', { timeout: 500 })
+      .should('exist');
+    cy.get('.step')
+      .last()
+      .next('.animate-pulse', { timeout: 5500 })
+      .should('not.exist');
+    cy.get('.step').should('have.length', 5);
     cy.get('.step').last().should('contain.text', 'Received message: Jeeves');
   });
 });
