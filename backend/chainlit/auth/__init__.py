eae5613b813efb2cf3ac94f3bcaa0a2d973b4187
xjose97x <joseignacioescudero@gmail.com>
2025-08-13 20:51:09 +0200

    refactor(auth): simplify require_login function to only check for CHAINLIT_AUTH_SECRET

--- backend/chainlit/auth/__init__.py
+++ backend/chainlit/auth/__init__.py
@@ -30,12 +30,7 @@ def is_oauth_enabled():
 
 
 def require_login():
-    return (
-        bool(os.environ.get("CHAINLIT_AUTH_SECRET"))
-        or config.code.password_auth_callback is not None
-        or config.code.header_auth_callback is not None
-        or is_oauth_enabled()
-    )
+    return bool(os.environ.get("CHAINLIT_AUTH_SECRET"))
 
 
 def get_configuration():
