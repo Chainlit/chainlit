7e57512132626c166748ee09085cab2afeb69665
josca <jonathanscharff@gmail.com>
2025-08-21 09:33:15 +0000

    Added shadcn chart component and added them as dependencies in imports.tsx

--- frontend/src/components/Elements/CustomElement/Imports.ts
+++ frontend/src/components/Elements/CustomElement/Imports.ts
@@ -1,6 +1,7 @@
 import * as LucideIcons from 'lucide-react';
 import React from 'react';
 import * as ReactHookForm from 'react-hook-form';
+import * as Recharts from 'recharts';
 import * as Recoil from 'recoil';
 import * as Sonner from 'sonner';
 import * as Zod from 'zod';
@@ -15,6 +16,7 @@ import * as BadgeComponents from '@/components/ui/badge';
 import * as ButtonComponents from '@/components/ui/button';
 import * as CardComponents from '@/components/ui/card';
 import * as CarouselComponents from '@/components/ui/carousel';
+import * as ChartComponents from '@/components/ui/chart';
 import * as CheckboxComponents from '@/components/ui/checkbox';
 import * as CommandComponents from '@/components/ui/command';
 import * as DialogComponents from '@/components/ui/dialog';
@@ -41,6 +43,7 @@ const Imports = {
   react: React,
   sonner: Sonner,
   zod: Zod,
+  recharts: Recharts,
   recoil: Recoil,
   '@chainlit/react-client': ChainlitReactClient,
   '@/components/markdown': Markdown,
@@ -54,6 +57,7 @@ const Imports = {
   '@/components/ui/button': ButtonComponents,
   '@/components/ui/card': CardComponents,
   '@/components/ui/carousel': CarouselComponents,
+  '@/components/ui/chart': ChartComponents,
   '@/components/ui/checkbox': CheckboxComponents,
   '@/components/ui/command': CommandComponents,
   '@/components/ui/dialog': DialogComponents,
