edb4e0f231792f94e9aacbffa3f8d6b07bb6b1af
hayescode <35790761+hayescode@users.noreply.github.com>
2025-09-15 10:31:57 -0500

    init

--- backend/chainlit/server.py
+++ backend/chainlit/server.py
@@ -1001,8 +1001,8 @@ async def get_shared_thread(
 
     is_shared = bool(metadata.get("is_shared"))
 
-    # Proceed only if both conditions are True.
-    if not (user_can_view and is_shared):
+    # Proceed only raise an error if both conditions are False.
+    if (not user_can_view) and (not is_shared):
         raise HTTPException(status_code=404, detail="Thread not found")
 
     metadata.pop("chat_profile", None)
