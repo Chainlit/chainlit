b0956d637bd64365cd580d335fbab7c556278c8c
hayescode <35790761+hayescode@users.noreply.github.com>
2025-09-24 21:29:38 -0500

    new

--- backend/chainlit/socket.py
+++ backend/chainlit/socket.py
@@ -173,12 +173,11 @@ async def connection_successful(sid):
     await context.emitter.clear("clear_ask")
     await context.emitter.clear("clear_call_fn")
 
-    if context.session.restored:
-        if config.code.on_chat_start and not context.session.has_first_interaction:
+    if context.session.restored and not context.session.has_first_interaction:
+        if config.code.on_chat_start:
             task = asyncio.create_task(config.code.on_chat_start())
             context.session.current_task = task
-        else:
-            return
+        return
 
     if context.session.thread_id_to_resume and config.code.on_chat_resume:
         thread = await resume_thread(context.session)
