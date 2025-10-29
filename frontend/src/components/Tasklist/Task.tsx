533e20b39103ece6e80067b55e794c6ead7b4228
Josh Hayes <35790761+hayescode@users.noreply.github.com>
2025-10-29 14:39:40 -0500

    Fix/tasklist (#2609)
    Ever since version 2.0.0 the tasks in the tasklist were disconnected
    from the Steps/Messages. I discovered that the connection was just
    completely missing so this adds it back in.
    
    closes: #2058 
    
    
    ![tasklist](https://github.com/user-attachments/assets/959d6988-90f5-4db6-9728-98e5ee46d097)

--- frontend/src/components/Tasklist/Task.tsx
+++ frontend/src/components/Tasklist/Task.tsx
@@ -24,12 +24,34 @@ export const Task = ({ index, task }: TaskProps) => {
     failed: 'text-muted-foreground'
   };
 
+  const handleClick = () => {
+    if (task.forId) {
+      const parent = document.getElementById(`step-${task.forId}`);
+      if (parent) {
+        // Find the child div below the main step container
+        const child = parent.querySelector('div');
+        if (child) {
+          child.classList.add('bg-card', 'rounded');
+          parent.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+            inline: 'start'
+          });
+          setTimeout(() => {
+            child.classList.remove('bg-card', 'rounded');
+          }, 600); // 2 blinks at 0.3s each
+        }
+      }
+    }
+  };
+
   return (
     <div className={`task task-status-${task.status}`}>
       <div
         className={`w-full flex font-medium py-2 text-sm leading-snug ${
           statusStyles[task.status]
-        }`}
+        } ${task.forId ? 'cursor-pointer' : 'cursor-default'}`}
+        onClick={handleClick}
       >
         <span className="flex-none w-8 pr-2">{index}</span>
         <TaskStatusIcon status={task.status} />
